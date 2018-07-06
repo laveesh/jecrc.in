1e25a3656ab37b3471263b83eb4588e0f61fd496
amritsrivastava <srivastava.amrit1999@gmail.com>
2018-07-06 20:36:45 +0530

    favicon added

--- src/index.php
+++ src/index.php
@@ -4,10 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Bootstrap 4 Starter Pack</title>
+  <title>JECRC</title>
   <link rel="stylesheet" href="css/font-awesome.min.css">
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" href="img/favicon.png" type="image/x-icon"/>
 </head>
 <body>
   <?php include '../modules/header.php'; ?>
