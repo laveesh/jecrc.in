ef2905e2a9b2b8af3c31622b4a8ec4198b766f76
prasang007 <prasang00@gmail.com>
2018-07-10 13:10:39 +0530

    add academics

--- departments/cse/modules/events.php
+++ departments/cse/modules/events.php
@@ -1,1 +1,135 @@
-6
+<section id="events">
+	<div class="container">
+		<div class="mainsectionHeading sectionHeading">Recents events @JECRC</div>
+		<section id="event1" class="card">			
+			<div class="sectionHeading">Expert talk on “Big Data” by Dr. Mansaf Alam from Jamia Milia Islamia, Delhi on 27th October,2015.</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel" data-slide-to="1"></li>
+						<li data-target="#events_carausel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image1.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image2.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12">
+					<ol>
+						<li><strong>Date of the event:</strong>27<sup>th</sup>October 2015.</li>
+						<li><strong>Venue:</strong>JSSATE Noida</li>
+						<li><strong>Objective/s:</strong>To introduce the significance and emergence of Big Data.</li>
+						<li><strong>Outcome:</strong>Candidates (Students and Faculty) will be able to identify and interpret concepts of Big Data.</li>
+						<li><strong>Guests of Honour at the event:</strong>Dr. Mansaf Alam.</li>
+					</ol>
+				</div>
+			</div>
+		</section>
+	<section id="event1" class="card">			
+			<div class="sectionHeading">Two days workshop on “Big Data & Hadoop” conducted by APPFLUXUS 2016 in collaboration with EDP Cell, IIT Roorkee.</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel2" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel2" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel2" data-slide-to="1"></li>
+						<li data-target="#events_carausel2" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image2.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image1.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel2" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel2" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12">
+				<ol>
+					<li><strong>Date of the event:</strong>4th November - 5th November, 2015.</li>
+					<li><strong>Venue:</strong>JSSATE, Noida</li>
+					<li><strong>Objective/s:</strong>To discuss the emergence and future prospects on &ldquo;Big Data and Hadoop&rdquo;.</li>
+					<li><strong>Outcome:</strong>Candidates will be able to analyze and interpret the possibilities and future scope w.r.t implementation on Bid Data and Hadoop.</li>
+				</ol>
+				</div>
+			</div>
+		</section>
+		<section id="event3" class="card">			
+			<div class="sectionHeading">Expert Talk on “Digital Disruption” by Mr. Prashant Pandey</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel3" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel3" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel3" data-slide-to="1"></li>
+						<li data-target="#events_carausel3" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image6.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image5.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="departments/cse/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel3" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel3" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12">
+				<ol>
+					<li><strong>Date of the event:</strong>4th November - 5th November, 2015.</li>
+					<li><strong>Venue:</strong>JSSATE, Noida</li>
+					<li><strong>Objective/s:</strong>To discuss the emergence and future prospects on &ldquo;Big Data and Hadoop&rdquo;.</li>
+					<li><strong>Outcome:</strong>Candidates will be able to analyze and interpret the possibilities and future scope w.r.t implementation on Bid Data and Hadoop.</li>
+				</ol>
+				<ol>
+					<li><strong>Objective/s:</strong>The objective is to provide a forum for faculty and students to share knowledge and experiences on implementing innovative digital strategies, team building, managing large-scale projects, leading the transformation of IT functions, leading strategic level change management and managing budgets</li>
+					<li><strong>Outcome:</strong>The target group will be able to learn implementation of innovative digital strategies, team building, managing large-scale projects, leading the transformation of IT functions, leading strategic level change management and managing budgets.</li>
+					<li><strong>Guests of Honour at the event:</strong>Mr. Prashant Pandey - Prashant Pandey is currently the Director of Content Development program at the State Library of Queensland. He leads the Content Strategy, Digital Strategy and Online Engagement Strategy for the Library. Prashant Pandey is an Alumni of JSSATEN; he belonged to the second Batch of engineering students from JSS and topped the Agra university in computer science &amp; engineering. Mr. Pandey is also a member of Department Advisory Committee ( DAC) and have contributed in the formation of vision and mission of the department</li>
+				</ol>
+				</div>
+			</div>
+		</section>
+		
+	</div>
+</section>
