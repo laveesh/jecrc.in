4c6cd9750364285b03e6d66cc4551421e760918d
Aditya Agarwal <aditya81070@gmail.com>
2018-07-11 16:00:01 +0530

    Aditya81070 about (#19)
    * Add about
    
    * Add contact info of cse
    
    * added history of cse
    
    * added hod's message
    
    * add mission and vision

--- departments/cse/modules/contact.php
+++ departments/cse/modules/contact.php
@@ -1,1 +1,10 @@
-4
+<section class="section container card">
+	<div class="sectionHeading">Contact Us</div>
+	<div class="sectionContent card-body">
+		<p>To request additional information concerning the Department of Computer Science and Engineering, you can reach us at: <a href="mailto:info.cse@jssaten.ac.in">info.cse@jssaten.ac.in</a></p>
+		<h4 class="sectionSubHeading">CSE Head</h4>
+		<p>AB-I First Floor</p>
+<p>Phone:(0120)2400104, Ext:  241 </p>
+<p><a href="mailto:hodcse@jssaten.ac.in">hodcse@jssaten.ac.in</a></p>
+	</div>
+</section>
