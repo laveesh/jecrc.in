6f9e17a3aca9c2d55e9c89944a8bf623867df4ca
Aditya Agarwal <aditya81070@gmail.com>
2018-07-11 10:38:46 +0530

    Aditya81070 patch 1 (#16)
    * Add Short discruption about people
    
    * Add faculty layout
    
    * side padding add for better mobile view
    
    * faculty layout changed

--- departments/cse/modules/faculty.php
+++ departments/cse/modules/faculty.php
@@ -1,1 +1,130 @@
-7
+<section class="section container">
+	<h1 class="sectionHeading">Department faculty</h1>
+<div class="row sectionContent">
+<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+	<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+	<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+
+</div>
+<div class="row sectionContent">
+<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+	<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+	<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+
+</div>
+<div class="row sectionContent">
+<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+	<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+	<div class="col-md-4 mt-4">
+		<div class="card">
+			<div class="card-header text-center"><h4>Name</h4></div>
+			<img src="departments/cse/src/img/people/faculty/aditya.jpg"class="card-img-top" alt="faculty-image">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
+				<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+				<li class="list-group-item">Block-A first floor room no-10</li>
+				<li class="list-group-item">random@someone.com</li>
+			</ul>
+
+		</div>
+	</div>
+
+</div>
+
+</section>
