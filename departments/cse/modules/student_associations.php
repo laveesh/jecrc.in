2b50bb0e3c9a0b545795f7dc79c0cf8789f72947
amritsrivastava <srivastava.amrit1999@gmail.com>
2018-07-11 11:00:18 +0530

    minor changes

--- departments/cse/modules/student_associations.php
+++ departments/cse/modules/student_associations.php
@@ -1,23 +1,23 @@
 <div class="container section">
 	<div class="sectionHeading"> STUDENT ASSOCIATIONS</div>
-	<div class="sectionSubHeading">CSI - Student Branch</div>
+	<h4 class="sectionSubHeading">CSI - Student Branch</h4>
 
 	<div class="sectionContent">Computer Society of India (CSI) formed in the year 1965 is one of the largest IT Professionals' Societies in India. The objective of the CSI is scientific and educational advancement in the theoretical and practical aspects of Computer Science and Engineering. It also facilitates holding meetings for presentation and discussion of professional papers, conferences, workshops and seminars.
 
 CSI Student Branch was inaugurated at JSS Academy of Technical Education, Noida on 25th February 2014. Since its start various workshops and lectures on topics such as Mobipreneur, IT Trends and Linux Operating System, and competitions and quizzes on C Programming, Mobile App Development and Basics of IT have been organized.</div>
 
-<div class="sectionSubHeading">Nibble Computer society</div>
+<h4 class="sectionSubHeading">Nibble Computer society</h4>
 
 <div class="sectionContent">The Nibble Computer Society is formed by students of CSE and IT working under the guidance of faculty. It is the oldest students' society of the institution. NCS organizes workshops, seminars and contests in order to inculcate technical and creative skills among the students. It is also actively involved in the design, development and maintenance of systems such as info-center, library system and the mail server. NCS consists of four clubs, each dealing with activities in a separate field of expertise . These are</div>
 
-<ul>
+<ul class="sectionList">
 	<li>Programming Club</li>
 	<li>Web club</li>
 	<li>linux club</li>
 	<li>Hardware club</li>
 </ul>
 
-<div class="sectionSubHeading">Google Developer Group(GDG)</div>
+<h4 class="sectionSubHeading">Google Developer Group(GDG)</h4>
 
 <div class="sectionContent">Students of CSE and IT have formed an open club - Google Developer Group . GDGs were started by Google as a part of their CSR (Corporate Social Responsibility) to spread awareness about the latest developments in technology. GDGs are for developers who are interested in Google's developer technology; everything from the Android, App Engine, and Google Chrome platforms, to product APIs like the Maps API, YouTube API and Google Calendar API. This group organizes workshops and lightning talks on Google technologies - APIs, SDKs, Web technologies, Platforms(Android), Programming languages, services(app engine) and so on.</div>
 </div>
