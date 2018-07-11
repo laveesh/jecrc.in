4f2ee6af9d46ded338471d684c2cdd97cbb3931b
Arpit jain <maiarpitjain94@gmail.com>
2018-07-11 10:47:14 +0530

    alumni section and student association (#17)
    * alumni section
    
    * create alumni and student assosiation
    
    * done changes

--- departments/cse/modules/alumni_notes.php
+++ departments/cse/modules/alumni_notes.php
@@ -1,1 +1,42 @@
-85
+<div class="container section">
+
+<div class="sectionHeading">ALUMNI NOTES</div>
+
+<div class="sectionContent">
+	<ul class="sectionList">
+			<dd class="pl-4">
+				<b>Degree year:2006-2010</b>
+			</dd>
+			<dd class="pl-4">
+				<b>Deepak N</b>
+			</dd>
+	</ul>	
+		<p>
+			"Along with building strong fundamentals in Computer Science, I also got hundreds of opportunities to build life skills like effective writing, public speaking, people and project management and leadership, all of which have been very useful in my career so far. My class- mates were confident and smart and added great value to my learning in the classroom. Because of the CS and Math skills I built at JSS, and the support of the faculty by way of strong recommendation letters, I got an admit to the Masters program in Computer Science at the State University of New York at Stony Brook. After finishing my Masters degree, I got a job at Microsoft Corp as a Software Development Engineer."
+		</p>
+</div>	
+
+
+<div class="sectionContent">
+	<ul class="sectionList">
+			<dd class="pl-4">
+				<b>Degree Year:2004-2008</b>
+			</dd>
+			<dd class="pl-4">
+				<b>Jolly Ahuja</b>
+			</dd>
+	
+			<dd class="pl-4">
+				Worked in IBM (2008-2013) as Business Manager
+
+			</dd >
+			<dd class="pl-4">
+				Currently pursuing MBA from S.P Jain.
+			</dd>
+	</ul>	
+
+		<p>
+			"JSS is one of the best college in Uttar Pradesh..It's infrastructure specially library is the best. Due to its location in Noida, it becomes recruitment center for many MNCs'. Special thanks to all the Professors ,Their hard work made and domain knowledge helped us a lot in our corporate life."
+		</p>
+</div>
+</div>
--- departments/cse/modules/support.php
+++ departments/cse/modules/support.php
@@ -1,1 +1,32 @@
-19
+<div class="container section">
+
+	<div class="sectionHeading">SUPPORT</div>
+
+
+<div class="sectionContent">To strengthen the interaction between JSS and alumni the official alumni association for the JSS Academy of Technical Education Noida, JSS Noida Alumni Association (JSSNAA), consisting of dedicated team of staff members headed by Dr Z.K. Ansari (HOD, Department of Mathematics), has been set up</div>
+
+<div class="sectionContent">In the current technologically advanced world, there are more and more ways to connect with our alumni. We always love to connect with our alumni, be it on mail, facebook or any other medium of contact. All alumni of the college are automatically members of JSSNAA.</div>
+
+<div class="sectionContent">For all queries alumni may contact</div>
+
+<ul class="sectionList">
+	<dd class="pl-4">Dr. Z.K. Ansari</dd>
+	<dd class="pl-4">Alumni Coordinator</dd>
+	<dd class="pl-4">JSSATE, C-20/1, Sector - 62, Noida, U.P.</dd>
+	<dd class="pl-4">E-mail: alumni@jssaten.ac.in</dd>
+	<dd class="pl-4">Contact No. 0120-2400104, 2400115,2401442, ext. 225</dd>
+	
+</ul>
+
+
+
+<ul class="sectionList">
+	<dd class="pl-4">Dr. Bhupender Parashar</dd>
+	<dd class="pl-4">Alumni Officer</dd>
+	<dd class="pl-4">JSS ATE, C-20/1, Sector - 62, Noida, U.P.</dd>
+	<dd class="pl-4">E-mail: alumni@jssaten.ac.in</dd>
+	<dd class="pl-4">Contact No. 0120-2400104, 2400115,2401442, ext. 225</dd>
+	
+</ul>
+
+</div>
