8e78cdf526bbacb4f737e7c1a67bac1c99d96d24
amritsrivastava <srivastava.amrit1999@gmail.com>
2018-07-12 00:40:16 +0530

    include files in ece dept

--- departments/ece/modules/awards_faculty.php
+++ departments/ece/modules/awards_faculty.php
@@ -1,1 +1,30 @@
-2
+<section id="awards_faculty" class="container card section">
+	<div class="sectionHeading">Faculty Awards</div>
+	<div class=" card-body sectionContent">
+	<div class="sectionSubHeading">Faculty Awards and Honours</div>
+	<ol class="sectionList" style="list-style-type: upper-roman;">
+<li><strong><em>Doctorate Awarded to Ms. Jyoti Gautam.</em></strong>The degree of Doctor of Philosophy was conferred to Ms. Jyoti Gautam on 12th April, 2016 by Gautam Buddha University (Established by UP Act No. 9 of 2002) based on the recommendations of the academic council of the university.</li>
+<li><strong><em>Selection in PhD and Other Technical Qualification :</em></strong>
+<ol class="sectionList" style="list-style-type: upper-roman;">
+<li>Mr. Rohit Agarwal selected for PhD program in IIT, Indore.</li>
+<li>Mr. Mahboob Alam selected for PhD program in Jamia Milia Islamia, Delhi.</li>
+<li>Mr. Sur Singh Rawat selected for PhD program in G.B.Pant Engineering College, Garhwal, Uttarakhand.</li>
+<li>Mr. Krishnanand Chaturvedi qualified JEST 2015 (Computer Science) conducted by IIMSc, Chennai.</li>
+<li>Mr. Krishnanand Chaturvedi ,Mr. Vimal Gupta and Mrs. Jaspreet Kaur selected by UPTU for PhD program in CSE.</li>
+</ol>
+</li>
+<li><strong><em>Ajay Kumar Verma attended:</em></strong>
+<ol class="sectionList" style="list-style-type: upper-roman;">
+<li>One Week Short term Course (FDP) by NITTTR Chandigarh on "MATLAB and its Hardware Interface" from 11-01-16 to 15-01-16 at JSSATE, Noida.</li>
+<li>Survey on the Methodologies to Improve the Scalability of SWS in International Journal of Advance Research in Engineering Science and Technology, ISSN (Online): 2393-9877, ISSN (print): 2394-2444, Volume 03, Issue 02,IF: 3.63,February-2016</li>
+<li>Participated in Two days workshop on<strong>Big Data  Hadoop</strong>conducted by APPFLUXUS 2016 in collaboration with EDP Cell, IIT Roorkee from 4th November - 5th November, 2015.</li>
+</ol>
+</li>
+<li>Ajay Kumar Verma and Ms. Gunjan Goswami attended a One Week Short term Course (FDP) on "Operation Research"conducted by NITTTR Chandigarh from 28-03-16 to 01-04-16 at JSSATE, Noida</li>
+<li>Ms. Urvashi Rahul Saxena, Ms. Jaspreet Kaur, Ajay Kumar Verma and Ms. Suruchi Saberwal attended a one week FDP on Students Evaluation from 16th Nov-15 to 20th Nov-15 by NITTTR Chandigarh conducted at JSSATE, Noida</li>
+<li>Ms. Urvashi Rahul Saxena and Ms. Suruchi Saberwal attended a two- week FDP on Research Methodology from 28th Dec-15 to 8th Jan-16 by NITTTR Chandigarh conducted at JSSATE, Noida.</li>
+<li>Ms. Bhavna Bansal and Mr. Girish Kumar B.C. participated in STC on ICT Based programme on Sustainable Environmental Management conducted on 14-18 March, 2016.</li>
+<li>Ms.Deepti Aggarwal, Ms. Sonali Mathur, Ms. Anita Sahoo attended ICT Based FDP on Effective Curriculum Implementation from 7th to 11th March, 2016 in collaboration with NITTTR Chandigarh in Seminar Hall in JSSATE, Noida.</li>
+</ol>
+</div>
+</section>
