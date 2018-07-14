ddf6177b744b7f5e5b98be143e3fce13a85018a6
amritsrivastava <srivastava.amrit1999@gmail.com>
2018-07-14 12:54:28 +0530

    pages directory add

---
+++ pages/about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>JECRC</title>
+  <link rel="stylesheet" href="../src/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../src/css/bootstrap.css">
+  <link rel="stylesheet" href="../src/css/style.css">
+  <link rel="icon" href="../src/img/favicon.png" type="image/x-icon"/>
+</head>
+<body>
+  <?php include '../modules/header.php'; ?>
+
+  <?php include '../modules/sidenav.php'; ?>
+
+  <?php include '../modules/footer.php'; ?>
+
+  <script src="../src/js/jquery.min.js"></script>
+  <script src="../src/js/popper.min.js"></script>
+  <script src="../src/js/bootstrap.min.js"></script>
+</body>
+</html>
