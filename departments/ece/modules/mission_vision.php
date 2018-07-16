c7a0e76bb357c0c1966826ff309efdfcf98b0a9c
amritsrivastava <srivastava.amrit1999@gmail.com>
2018-07-16 09:15:56 +0530

    fix typo

--- departments/ece/modules/mission_vision.php
+++ departments/ece/modules/mission_vision.php
@@ -6,7 +6,7 @@
     </p>
     <h3 class="sectionSubHeading">Mission</h3>
     <ol class="list-group list-group-flush sectionList">
-      <li class="list-group-item py-3">To equip the students with astrong foundation of basic sciences and domain knowledge of ECE, so that they are able to creatively apply their knowledge to the solution arising in their career path.</li>
+      <li class="list-group-item py-3">To equip the students with a strong foundation of basic sciences and domain knowledge of ECE, so that they are able to creatively apply their knowledge to the solution arising in their career path.</li>
       <li class="list-group-item py-3">To induce the habit of life-long learning to enhance overall performance.
       </li>
       <li class="list-group-item py-3">Students are able to communicate their ideas clearly and concisely so that they can work in a team as well as an individual.
