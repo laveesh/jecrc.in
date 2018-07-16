7fff58998e92994c2d7588c2073a599d878fb864
aish51197 <31503134+aish51197@users.noreply.github.com>
2018-07-16 09:13:50 +0530

    Aishwarya patch 1 (#26)
    * about updated
    
    * Update Hod message
    
    *  Update mission and vison
    
    * Update Hod image

--- departments/ece/modules/mission_vision.php
+++ departments/ece/modules/mission_vision.php
@@ -1,15 +1,19 @@
 <section class="section container card">
-	<div class="sectionHeading">Mission and Vision</div>
-	<div class="sectionContent card-body">
-	<h3 class="sectionSubHeading">Vision</h3>
-	<p>To spark the imagination of the Computer Science Engineers with values, skills and creativity to solve the real world problems.</p>
-	<h3 class="sectionSubHeading">Mission
-		</h3>
-		<ol class="list-group list-group-flush sectionList">
-			<li class="list-group-item py-3">To inculcate creative thinking and problem solving skills through effective teaching, learning and research.</li>
-			<li class="list-group-item py-3">To empower professionals with core competency in the field of Computer Science and Engineering.</li>
-			<li class="list-group-item py-3">To foster independent and lifelong learning with ethical and social responsibilities.</li>
-		</ol>
-	
-	</div>
+  <div class="sectionHeading">Mission and Vision</div>
+  <div class="sectionContent card-body">
+    <h3 class="sectionSubHeading">Vision</h3>
+    <p>To contribute through excellence in scientific and technical education, teaching and research in Electronics and Communication Engineering & to meet the needs of global industry.
+    </p>
+    <h3 class="sectionSubHeading">Mission</h3>
+    <ol class="list-group list-group-flush sectionList">
+      <li class="list-group-item py-3">To equip the students with astrong foundation of basic sciences and domain knowledge of ECE, so that they are able to creatively apply their knowledge to the solution arising in their career path.</li>
+      <li class="list-group-item py-3">To induce the habit of life-long learning to enhance overall performance.
+      </li>
+      <li class="list-group-item py-3">Students are able to communicate their ideas clearly and concisely so that they can work in a team as well as an individual.
+      </li>
+      <li class="list-group-item py-3">To make students responsive towards the ethical, social, environmental and economic context for the society.
+      </li>
+    </ol>
+
+  </div>
 </section>
