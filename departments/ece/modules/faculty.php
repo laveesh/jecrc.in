87607812cb7cb0596c9d4db966bc99ca6fba8a5a
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-07-17 11:52:56 +0530

    halfway ece faculty insert

--- departments/ece/modules/faculty.php
+++ departments/ece/modules/faculty.php
@@ -3,19 +3,18 @@
 <div class="row sectionContent">
 	<div class="col-md-6 mt-4">
 				<div class="panel-body card">
-					<div class="card-header text-center"><h4>Name</h4></div>
+					<div class="card-header text-center"><h4>Anil Jain</h4></div>
 					<div class="row">
 						<div class="col-md-4 align="center">
 							<img src="../departments/ece/src/img/people/faculty/random.jpg"class="img-circle img-responsive" alt="faculty-image">
 						</div>
 						<div class="col-md-8 px-2">
 							<ul class="list-group list-group-flush">
-								<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
-								<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+								<li class="list-group-item"><strong>Educational Qualification</strong>: M.Tech. (Communication and Signal Processing)</li>
+								<li class="list-group-item"><strong>Designation</strong>: Assistant Professor</li>
 								<li class="list-group-item">Block-A first floor room no-10</li>
-								<li class="list-group-item">random@someone.com</li>
-								<li class="list-group-item">Experience: 4year</li>
-								<li class="list-group-item">Publication: 0</li>
+								<li class="list-group-item">aniljain.ece@jecrc.ac.in</li>
+								<li class="list-group-item">Experience: 8years</li>
 							</ul>
 						</div>
 					</div>
@@ -24,19 +23,18 @@
 	
 	<div class="col-md-6 mt-4">
 				<div class="panel-body card">
-					<div class="card-header text-center"><h4>Name</h4></div>
+					<div class="card-header text-center"><h4>Aapurva Kaul</h4></div>
 					<div class="row">
 						<div class="col-md-4 align="center">
 							<img src="../departments/ece/src/img/people/faculty/random.jpg"class="img-circle img-responsive" alt="faculty-image">
 						</div>
 						<div class="col-md-8 px-2">
 							<ul class="list-group list-group-flush">
-								<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
-								<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+								<li class="list-group-item"><strong>Educational Qualification</strong>: M.TECH	(VLSI DESIGN)</li>
+								<li class="list-group-item"><strong>Designation</strong>: Assistant Professor</li>
 								<li class="list-group-item">Block-A first floor room no-10</li>
-								<li class="list-group-item">random@someone.com</li>
-								<li class="list-group-item">Experience: 4year</li>
-								<li class="list-group-item">Publication: 0</li>
+								<li class="list-group-item">aapurvakaul.ece@jecrc.ac.in</li>
+								<li class="list-group-item">Experience: 9 MONTHS</li>
 							</ul>
 						</div>
 					</div>
@@ -47,19 +45,18 @@
 <div class="row sectionContent">
 	<div class="col-md-6 mt-4">
 				<div class="panel-body card">
-					<div class="card-header text-center"><h4>Name</h4></div>
+					<div class="card-header text-center"><h4>ANKUR GANGWAR</h4></div>
 					<div class="row">
 						<div class="col-md-4 align="center">
 							<img src="../departments/ece/src/img/people/faculty/random.jpg"class="img-circle img-responsive" alt="faculty-image">
 						</div>
 						<div class="col-md-8 px-2">
 							<ul class="list-group list-group-flush">
-								<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
-								<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+								<li class="list-group-item"><strong>Educational Qualification</strong>: M.Tech.(Communication Systems and Networks)</li>
+								<li class="list-group-item"><strong>Designation</strong>: ASSISTANT PROFESSOR</li>
 								<li class="list-group-item">Block-A first floor room no-10</li>
-								<li class="list-group-item">random@someone.com</li>
-								<li class="list-group-item">Experience: 4year</li>
-								<li class="list-group-item">Publication: 0</li>
+								<li class="list-group-item">ankurgangwar.ece@jecrc.ac.in</li>
+								<li class="list-group-item">Experience: 5 year 6 month</li>
 							</ul>
 						</div>
 					</div>
@@ -68,19 +65,18 @@
 	
 	<div class="col-md-6 mt-4">
 				<div class="panel-body card">
-					<div class="card-header text-center"><h4>Name</h4></div>
+					<div class="card-header text-center"><h4>ASHISH KULSHRESTHA</h4></div>
 					<div class="row">
 						<div class="col-md-4 align="center">
 							<img src="../departments/ece/src/img/people/faculty/random.jpg"class="img-circle img-responsive" alt="faculty-image">
 						</div>
 						<div class="col-md-8 px-2">
 							<ul class="list-group list-group-flush">
-								<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
-								<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+								<li class="list-group-item"><strong>Educational Qualification</strong>: M.Tech (Digital Communication)</li>
+								<li class="list-group-item"><strong>Designation</strong>: Assistant Professor</li>
 								<li class="list-group-item">Block-A first floor room no-10</li>
 								<li class="list-group-item">random@someone.com</li>
-								<li class="list-group-item">Experience: 4year</li>
-								<li class="list-group-item">Publication: 0</li>
+								<li class="list-group-item">Experience: 6 years</li>
 							</ul>
 						</div>
 					</div>
@@ -91,19 +87,18 @@
 <div class="row sectionContent">
 	<div class="col-md-6 mt-4">
 				<div class="panel-body card">
-					<div class="card-header text-center"><h4>Name</h4></div>
+					<div class="card-header text-center"><h4>ASHUTOSH SHARMA</h4></div>
 					<div class="row">
 						<div class="col-md-4 align="center">
 							<img src="../departments/ece/src/img/people/faculty/random.jpg"class="img-circle img-responsive" alt="faculty-image">
 						</div>
 						<div class="col-md-8 px-2">
 							<ul class="list-group list-group-flush">
-								<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
-								<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+								<li class="list-group-item"><strong>Educational Qualification</strong>: M.Tech.(VLSI Design)</li>
+								<li class="list-group-item"><strong>Designation</strong>: Assistant Professor</li>
 								<li class="list-group-item">Block-A first floor room no-10</li>
-								<li class="list-group-item">random@someone.com</li>
-								<li class="list-group-item">Experience: 4year</li>
-								<li class="list-group-item">Publication: 0</li>
+								<li class="list-group-item">ashutoshsharma.ece@jecrc.ac.in</li>
+								<li class="list-group-item">Experience: 3 years</li>
 							</ul>
 						</div>
 					</div>
@@ -112,19 +107,18 @@
 	
 	<div class="col-md-6 mt-4">
 				<div class="panel-body card">
-					<div class="card-header text-center"><h4>Name</h4></div>
+					<div class="card-header text-center"><h4>BHOOPESH KUMAR KUMAWAT</h4></div>
 					<div class="row">
 						<div class="col-md-4 align="center">
 							<img src="../departments/ece/src/img/people/faculty/random.jpg"class="img-circle img-responsive" alt="faculty-image">
 						</div>
 						<div class="col-md-8 px-2">
 							<ul class="list-group list-group-flush">
-								<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
-								<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+								<li class="list-group-item"><strong>Educational Qualification</strong>: M. TECH. (DIGITAL COMMUNICATION)</li>
+								<li class="list-group-item"><strong>Designation</strong>: ASSISTANT PROFESSOR</li>
 								<li class="list-group-item">Block-A first floor room no-10</li>
-								<li class="list-group-item">random@someone.com</li>
-								<li class="list-group-item">Experience: 4year</li>
-								<li class="list-group-item">Publication: 0</li>
+								<li class="list-group-item">bhoopesh.ece@jecrc.ac.in</li>
+								<li class="list-group-item">Experience: 11 years 6 Months</li>
 							</ul>
 						</div>
 					</div>
@@ -135,19 +129,18 @@
 <div class="row sectionContent">
 	<div class="col-md-6 mt-4">
 				<div class="panel-body card">
-					<div class="card-header text-center"><h4>Name</h4></div>
+					<div class="card-header text-center"><h4>Deepak Sankhala</h4></div>
 					<div class="row">
 						<div class="col-md-4 align="center">
 							<img src="../departments/ece/src/img/people/faculty/random.jpg"class="img-circle img-responsive" alt="faculty-image">
 						</div>
 						<div class="col-md-8 px-2">
 							<ul class="list-group list-group-flush">
-								<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
-								<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+								<li class="list-group-item"><strong>Educational Qualification</strong>: M.Tech (Electronics communication)</li>
+								<li class="list-group-item"><strong>Designation</strong>: Assistant Professor</li>
 								<li class="list-group-item">Block-A first floor room no-10</li>
-								<li class="list-group-item">random@someone.com</li>
-								<li class="list-group-item">Experience: 4year</li>
-								<li class="list-group-item">Publication: 0</li>
+								<li class="list-group-item">deepak.ece@jecrc.ac.in</li>
+								<li class="list-group-item">Experience: 8 years</li>
 							</ul>
 						</div>
 					</div>
@@ -156,19 +149,18 @@
 	
 	<div class="col-md-6 mt-4">
 				<div class="panel-body card">
-					<div class="card-header text-center"><h4>Name</h4></div>
+					<div class="card-header text-center"><h4>ASHUTOSH SHARMA</h4></div>
 					<div class="row">
 						<div class="col-md-4 align="center">
 							<img src="../departments/ece/src/img/people/faculty/random.jpg"class="img-circle img-responsive" alt="faculty-image">
 						</div>
 						<div class="col-md-8 px-2">
 							<ul class="list-group list-group-flush">
-								<li class="list-group-item"><strong>Educational Qualification</strong>: PhD</li>
-								<li class="list-group-item"><strong>Designation</strong>: Lecturer</li>
+								<li class="list-group-item"><strong>Educational Qualification</strong>: M.Tech. (VLSI Design)</li>
+								<li class="list-group-item"><strong>Designation</strong>: Assistant Professor</li>
 								<li class="list-group-item">Block-A first floor room no-10</li>
-								<li class="list-group-item">random@someone.com</li>
-								<li class="list-group-item">Experience: 4year</li>
-								<li class="list-group-item">Publication: 0</li>
+								<li class="list-group-item">ashutoshsharma.ece@jecrc.ac.in</li>
+								<li class="list-group-item">Experience: 8 years</li>
 							</ul>
 						</div>
 					</div>
