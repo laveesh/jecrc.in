5367d59d2897874d912cdf916ad65d8c278177ea
Prasang Gupta <31915135+Prasang007@users.noreply.github.com>
2018-07-18 14:53:17 +0530

    update event details (#31)
    * update event details
    
    * added more events completed student oppurtunities
    
    * deleted 258mb images

--- departments/ece/modules/student_opportunities.php
+++ departments/ece/modules/student_opportunities.php
@@ -1,28 +1,6 @@
 <section id="StudentOpportunities" class="container card section">
 	<div class="sectionHeading">Student Opportunities</div>
 	<div class="card-body sectionContent">
-<p >The Department strives to provide opportunities to the students by creating a stimulating environment to strengthen technical knowledge as well as honing soft skills. It works relentlessly to endow them with competence in any and every sphere of technical education.</p>
-<p >The students get opportunities to participate in many workshops and trainings and get certificates through the alliance with IBM, SAP and Nokia. Since its launch in November 2009, the IBM Center of Excellence has provided many opportunities for students to learn IBM Technologies and work on them through project events held by IBM. Over the years more than 1000 students have undergone training and have been certified in various IBM Technologies such as DB2, RAD (Rational Application Developer), RTC (Rational Team Concert), WID (Webshpere Integrator Developer), TDS (Tivoli Directory Server) and Lotus Domino Server. More than 100 students have received training and certificates in SAP since the Department became a SAP University Alliance Member in 2013. Opportunities in Mobile Application Development are being provided through the Nokia Research Labs.</p>
-<p >The students get an opportunity to exhibit their skills and knowledge in various co-curricular and extra-curricular areas through competitions and events organized on a regular basis. Examples of some such events are</p>
-<ol class="sectionList">
-<li>Zealicon which is an annual technical and cultural festival.</li>
-<li>Activities organized by Nibble Computer Society</li>
-<li>Activities organized by Google Developer Group</li>
-</ol>
+<p>Our main goal is to make open doors for the undergraduates to understand their Skill and potential. Toadvance the knowledge for the innovative and social devlpoement of the society. To reinforce theconnection amongst the scholarly world and industry.There is several opportunity in the market for Electronics and communication enginners. They are jus lagbehind in Skills, aptitude and the training. The Entry Level package for the Freshers starts with 2.5 to 3.5lakh per annum. But the Brilliant students with their outstanding knowledge in the Field of EmbeddedSystem, VLSI design and Communication technology can go far beyond average package.Electronics and Communication engineering is one of the most popular engineering branches out there.Among the Indian engineering aspirants, this particular discipline has been a popular choice for quitesome time.With the evolution of the computer age, electronics communication has transformed intoa field that is inevitable, required by almost all the industries.Electronics is now part of our everyday life, from the mobile phones to televisions, computers and eventhe high-end advanced satellites that are helping us to lead a smooth life.Even electronics andcommunication engineering is the major driving force for the present day Information Technologyrevolution.Electronics and Communication Engineering has also penetrated into other areas like healthcare,instrumentation, automation, remote sensing, signal processing etc. So students pursuing electronicsand communication engineering have a lot of scope in varied industries.</p>
 </div>
 </section>
-<section id="employmentAvenues" class="container card section">
-	<div class="sectionHeading">Employment Avenues</div>
-	<div class="card-body sectionContent">
-	<p >Students are given career opportunities in the coveted areas of research and development, software industries, banking sectors, information security and other suitable areas. To facilitate the placement process for the students, a department faculty placement coordinator, assisted by two student coordinators, monitor placement activities like pre-placement talks, online tests (as required by the company), interviews and GDs. Students are constantly notified about new companies and off-campus jobs in other colleges through notices and announcements. Information regarding company profile, packages offered, selection criteria and dates of visit is provided to the students well in advance and subsequently the student data is given to the placement offices according to their respective criteria.</p>
-<p >Students have been placed in renowned companies like Adobe, Infosys, Birlasoft, HCL and the likes with impressive salary packages ranging from 4 L p.a to as high as 8.3L p.a.</p>
-</div>
-</section>
-<section id="innovation" class="container card section">
-	<div class="sectionHeading">Innovation</div>
-	<div class="card-body sectionContent">
-	<p >One of the most used systems of the organization is the "JSS Info-Center". This system serves as a means of communication between faculty, staff, students and administrative staff. Notices, assignments and other important information can be shared through the info-center. This system was created by the students of CSE and IT and is also maintained by them.</p>
-	<p >Zealicon is a techno-cultural fest of the Academy which is held annually. Right from designing of the brochure for the fest till designing, developing, hosting and maintaining the website is done by the students of CSE and IT.</p>
-	<p >students of CSE and IT have also developed an online quiz system. This is used extensively by the faculty.</p>
-	</div>
-</section>	
