4ae00fabb1c8733923e3a97eecb1a637b8feeebb
Prasang Gupta <31915135+Prasang007@users.noreply.github.com>
2018-07-19 22:36:13 +0530

    some events completed (#39)

--- departments/ece/modules/tech_events.php
+++ departments/ece/modules/tech_events.php
@@ -1,7 +1,7 @@
 	
 	<div class="container">
 		<div class="sectionHeading">Recents events @JECRC</div>
-		<section class="section">
+		<!-- <section class="section">
 			<div class="sectionHeading">ROBOWAR</div>
 			<div class="row">
 				<div class="col-md-3 col-sm-12">
@@ -32,7 +32,7 @@
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>This competition is to show how long robot of yours can withstand in the arena fighting the other robot. Teams
 							must build a manually controlled machine which can withstand the other robot in the arena.
 							The bot can be wired or wireless. In case the participants use wireless mechanism, they must use dual
@@ -58,7 +58,7 @@
 					</ol>
 				</div>
 			</div>
-		</section>
+		</section> -->
 		<section class="section">
 			<div class="sectionHeading">ROBO SOCCER</div>`
 			<div class="row">
@@ -68,17 +68,35 @@
 						<li data-target="#events_carausel2" data-slide-to="0" class="active"></li>
 						<li data-target="#events_carausel2" data-slide-to="1"></li>
 						<li data-target="#events_carausel2" data-slide-to="2"></li>
+						<li data-target="#events_carausel2" data-slide-to="3"></li>
+						<li data-target="#events_carausel2" data-slide-to="4"></li>
+						<li data-target="#events_carausel2" data-slide-to="5"></li>
+						<li data-target="#events_carausel2" data-slide-to="6"></li>
+						<li data-target="#events_carausel2" data-slide-to="7"></li>
 					</ol>
 					<div class="carousel-inner">
 						<div class="carousel-item active">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/robosoccer.jpg" alt="First slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/robosoccer/1.jpg" alt="First slide">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/robosoccer/2.jpg" alt="Second slide">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/robosoccer/3.jpg" alt="Third slide">
 						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/robosoccer/4.jpg" alt="Fourth slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/robosoccer/5.jpg" alt="Fifth slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/robosoccer/6.jpg" alt="Sixth slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/robosoccer/7.jpg" alt="Seventh slide">
+						</div>
+
 					</div>
 					<a class="carousel-control-prev" href="#events_carausel2" role="button" data-slide="prev">
 					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -90,7 +108,7 @@
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p> For all those who have passion in Robotics or Sports, RENAISSANCE’18 presents robo-soccer. This competition is to show how good and swift robot of yours can be by playing soccer using it and scoring more goals.</p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>25<sup>th</sup>Febuary 2018.</li>
@@ -123,16 +141,20 @@
 						<li data-target="#events_carausel3" data-slide-to="0" class="active"></li>
 						<li data-target="#events_carausel3" data-slide-to="1"></li>
 						<li data-target="#events_carausel3" data-slide-to="2"></li>
+						<li data-target="#events_carausel3" data-slide-to="3"></li>
 					</ol>
 					<div class="carousel-inner">
 						<div class="carousel-item active">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/formula-zero/1.jpg" alt="formula-zer">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/formula-zero/2.jpg" alt="formula-zero">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/formula-zero/3.jpg" alt="formula-zer">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/formula-zero/4.jpg" alt="formula-zero">
 						</div>
 					</div>
 					<a class="carousel-control-prev" href="#events_carausel3" role="button" data-slide="prev">
@@ -145,7 +167,7 @@
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>For all those who have passion in Robotic so far Sports, RENAISSANCE͛’18 presents Formula zero. This competition  is to show how good and swift robot of yours can be,by crossing the hurdles using it and scoring more points.</p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>27<sup>th</sup>March 2018.</li>
@@ -178,16 +200,24 @@
 						<li data-target="#events_carausel4" data-slide-to="0" class="active"></li>
 						<li data-target="#events_carausel4" data-slide-to="1"></li>
 						<li data-target="#events_carausel4" data-slide-to="2"></li>
+						<li data-target="#events_carausel4" data-slide-to="3"></li>
+						<li data-target="#events_carausel4" data-slide-to="4"></li>
 					</ol>
 					<div class="carousel-inner">
 						<div class="carousel-item active">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/line-follower/1.jpg" alt="line-follower">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/line-follower/2.jpg" alt="line-followere">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/line-follower/3.jpg" alt="line-follower">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/line-follower/4.jpg" alt="line-follower">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/line-follower/5.jpg" alt="line-follower">
 						</div>
 					</div>
 					<a class="carousel-control-prev" href="#events_carausel4" role="button" data-slide="prev">
@@ -200,7 +230,7 @@
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>For all those who have passion in Robotics or Sports, RENAISSANCE’18 presents Line follower. This competition is to show how good and swift robot of yours can be by following a line using it and scoring more points.</p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>8<sup>th</sup>March 2018.</li>
@@ -233,16 +263,20 @@
 						<li data-target="#events_carausel5" data-slide-to="0" class="active"></li>
 						<li data-target="#events_carausel5" data-slide-to="1"></li>
 						<li data-target="#events_carausel5" data-slide-to="2"></li>
+						<li data-target="#events_carausel5" data-slide-to="3"></li>
 					</ol>
 					<div class="carousel-inner">
 						<div class="carousel-item active">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/drones/1.jpg" alt="games-of-drones">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/drones/2.jpg" alt="games-of-dronese">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/drones/3.jpg" alt="games-of-drones">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/drones/4.jpg" alt="games-of-drones">
 						</div>
 					</div>
 					<a class="carousel-control-prev" href="#events_carausel5" role="button" data-slide="prev">
@@ -255,7 +289,7 @@
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>Build your Drone to travel from source to destination crossing various interruptions, hurdles, etc., along the path of journey. This is an open event for all (but should not be from any industry or a professional).</p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>26<sup>th</sup>March 2018.</li>
@@ -302,7 +336,7 @@
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>This event is to develop research aptitude among engineering students. Future of education belongs to research. Students who want to pursue higher studies in india or abroad are very much inquisitive about  developing research aptitude . Technophilia will provide them a solid platform to begin. </p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>26<sup>th</sup>March 2017.</li>
@@ -353,7 +387,7 @@
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>“CASH THE ASH”, In this event students have to develop a working model from the available components of a non working or discarded device according their innovative idea. Creating is not finding of a thing but making something out of it after it is found so get your mind work to create something innovative from the ASH.  </p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>26<sup>th</sup>March 2018.</li>
@@ -383,16 +417,28 @@
 						<li data-target="#events_carausel8" data-slide-to="0" class="active"></li>
 						<li data-target="#events_carausel8" data-slide-to="1"></li>
 						<li data-target="#events_carausel8" data-slide-to="2"></li>
+						<li data-target="#events_carausel8" data-slide-to="3"></li>
+						<li data-target="#events_carausel8" data-slide-to="4"></li>
+						<li data-target="#events_carausel8" data-slide-to="5"></li>
 					</ol>
 					<div class="carousel-inner">
 						<div class="carousel-item active">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/renovators/1.jpg" alt="renovators">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/renovators/2.jpg" alt="renovators">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/renovators/3.jpg" alt="renovators">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/renovators/4.jpg" alt="renovators">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/renovators/5.jpg" alt="renovators">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/renovators/6.jpg" alt="renovators">
 						</div>
 					</div>
 					<a class="carousel-control-prev" href="#events_carausel8" role="button" data-slide="prev">
@@ -405,7 +451,7 @@
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p> Renovators is a technical event that falls right in into the core of engineering. It emphasis on the basic circuits that are involve in the course of engineering. It also disperse light onto the innovation and creativity. 
 There are 2 rounds, first about the identification of circuit elements and second about the design of already studied circuit. </p>
 					<ol class="sectionList">
@@ -461,7 +507,7 @@ There are 2 rounds, first about the identification of circuit elements and secon
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>Techino Buzz is a Technical poster presentation. This is the presentation of research information or an idea with an academic or professional focus on a poster. For any professional, communicating his/her idea efficiently determines his success professionally. The primary motive of this competition is to enhance the ability to deliver a visual presentation without any other audio/video aids. This event basically focuses on the creativity of the students. Here at Renaissance we provide you a platform to showcase your innovation, creativity, knowledge, and ability to tackle practical problems.</p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>27<sup>th</sup>March 2018.</li>
@@ -494,16 +540,24 @@ There are 2 rounds, first about the identification of circuit elements and secon
 						<li data-target="#events_carausel10" data-slide-to="0" class="active"></li>
 						<li data-target="#events_carausel10" data-slide-to="1"></li>
 						<li data-target="#events_carausel10" data-slide-to="2"></li>
+						<li data-target="#events_carausel10" data-slide-to="3"></li>
+						<li data-target="#events_carausel10" data-slide-to="4"></li>
 					</ol>
 					<div class="carousel-inner">
 						<div class="carousel-item active">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/quizolic/1.jpg" alt="quizolic">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/quizolic/2.jpg" alt="quizolic">
 						</div>
 						<div class="carousel-item">
-							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/quizolic/3.jpg" alt="quizolic">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/quizolic/4.jpg" alt="quizolic">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/quizolic/5.jpg" alt="quizolic">
 						</div>
 					</div>
 					<a class="carousel-control-prev" href="#events_carausel10" role="button" data-slide="prev">
@@ -516,7 +570,7 @@ There are 2 rounds, first about the identification of circuit elements and secon
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>A Quiz Is More Than Just A Collection Of Questions In Search Of Answers. It Is A Reflection Of The Times We Live And Have Lived In. The Event Presents A Medley Of Well Gleaned Technical Questions To Test The Intellectual Capabilities. If Archie Was The First And Backrub Became Google, The Advent Of Search Engines And Internet Has Meant That Answers To Anything Are Just A Click Away.</p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>27<sup>th</sup>March 2018.</li>
@@ -570,7 +624,7 @@ There are 2 rounds, first about the identification of circuit elements and secon
 					</a>
 					</div>
 				</div>
-				<div class="col-md-9 col-sm-12 sectionContent">
+				<div class="col-md-9 col-sm-12 sectionContent px-4">
 					<p>Combine your luck and your skills, solve basic mathematical equations and circuits and play Tambola. Improve your skills, solve time based questions and win exciting prizes.</p>
 					<ol class="sectionList">
 						<li><strong>Date of the event:</strong>25<sup>th</sup>March 2018.</li>
