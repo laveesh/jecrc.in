52392557535e6b9b074d78121771c82a50c7c6eb
aish51197 <31503134+aish51197@users.noreply.github.com>
2018-07-20 09:45:56 +0530

    Aishwarya patch 1 (#41)
    * update only ME tech-events
    
    * update ECE publication
    
    * update EE publications
    
    * update ME publications

--- departments/ece/modules/publication.php
+++ departments/ece/modules/publication.php
@@ -2,19 +2,60 @@
 <section class="section container card">
   <div class="sectionHeading">Publications</div>
   <div class="sectionContent card-body px-4">
-    <p><b>Aditi Gupta, Ajay Kumar, Jyoti Gautam</b>- Aditi Gupta, Ajay Kumar, Jyoti Gautam, â€œA Survey on Semantic Similarity Measuresâ€, in International Journal for Innovative Research in Science & Technology (IJIRST), Volume 3, Issue 12, May 2017.,Journal,05/22
+    <p><strong>Dr. Lokesh Bansal</strong>- Performance of S-T Block Coded MC-CDMA System.
+
     </p>
-    <p><b>Aditi Gupta, Ajay Kumar, Jyoti Gautam</b>- Aditi Gupta, Ajay Kumar, Jyoti Gautam, â€œA Survey on Semantic Similarity Measuresâ€, in International Journal for Innovative Research in Science & Technology (IJIRST), Volume 3, Issue 12, May 2017.,Journal,05/22
+    <p><strong>Dr. Lokesh Bansal</strong>- Review of Telecommunication Systems and Networks.
     </p>
-    <p><b>Aditi Gupta, Ajay Kumar, Jyoti Gautam</b>- Aditi Gupta, Ajay Kumar, Jyoti Gautam, â€œA Survey on Semantic Similarity Measuresâ€, in International Journal for Innovative Research in Science & Technology (IJIRST), Volume 3, Issue 12, May 2017.,Journal,05/22
+    <p><strong>Mrs. Shruti Kalra</strong>-Advances in all-optical switching.</p>
+    <p><strong>Mrs. Shruti Kalra</strong>-Performance analysis of the photonic crystal based structure for large scale photonic integrated circuits.</p>
+    <p><strong>Mrs. Shruti Kalra</strong>-Design and analysis of ultra-compact optical switch using phase change material.</p>
+    <p><strong>Mr. Anil Jain</strong>- Wind Energy Generation – Challenges & Developments
     </p>
-    <p><b>Aditi Gupta, Ajay Kumar, Jyoti Gautam</b>- Aditi Gupta, Ajay Kumar, Jyoti Gautam, â€œA Survey on Semantic Similarity Measuresâ€, in International Journal for Innovative Research in Science & Technology (IJIRST), Volume 3, Issue 12, May 2017.,Journal,05/22
+    <p><strong>Mr. Anil Jain</strong>-Leveraging IT for Transportation – Review & Analysis.
     </p>
-    <p><b>Aditi Gupta, Ajay Kumar, Jyoti Gautam</b>- Aditi Gupta, Ajay Kumar, Jyoti Gautam, â€œA Survey on Semantic Similarity Measuresâ€, in International Journal for Innovative Research in Science & Technology (IJIRST), Volume 3, Issue 12, May 2017.,Journal,05/22
+    <p><strong>Mr. Anil Jain</strong>-Wireless communication using visible light - future technology.
     </p>
-    <p><b>Aditi Gupta, Ajay Kumar, Jyoti Gautam</b>- Aditi Gupta, Ajay Kumar, Jyoti Gautam, â€œA Survey on Semantic Similarity Measuresâ€, in International Journal for Innovative Research in Science & Technology (IJIRST), Volume 3, Issue 12, May 2017.,Journal,05/22
+    <p><strong>Mr. Anil Jain</strong>- Advanced digital wireless liquid level indicator.
     </p>
-    <p><b>Aditi Gupta, Ajay Kumar, Jyoti Gautam</b>- Aditi Gupta, Ajay Kumar, Jyoti Gautam, â€œA Survey on Semantic Similarity Measuresâ€, in International Journal for Innovative Research in Science & Technology (IJIRST), Volume 3, Issue 12, May 2017.,Journal,05/22
+    <p><strong>Mr. Anil Jain</strong>- Design and comparision of the rectangular microstrip patch antenna.
     </p>
+    <p><strong>Mrs. Vinita Mathur</strong>- A new printed fractal right angled isosceles triangular
+  monopole antenna for ultra-wideband applications</p>
+    <p><strong>Mrs. Vinita Mathur</strong>-Planar Array of right angled isosceles triangular microstrip antenna using Koch and meander lines for
+      internet of things. </p>
+    <p><strong>Mrs. Vinita Mathur</strong>-Sierpinski Array with Swastik Electromagnetic Bandgap for Ku- Band Applications. </p>
+    <p><strong>Mrs. Vinita Mathur</strong>-Analysis of Right angled Isosceles triangular Microstrip
+    Patch Antenna (RITMA) for UWB Applications.</p>
+    <p><strong>Mrs. Vinita Mathur</strong>-Koch fractal antenna using right angled isosceles triangular
+      microstrip patch antenna structure for WiMAX. </p>
+    <p><strong>Mrs. Vinita Mathur</strong>-Design of compact Triangular Patch Antenna for WiMax
+     Applications .</p>
+    <p><strong>Mrs. Vinita Mathur</strong>-A Novel Square Patch Based Microstrip Antenna Design for
+     ISM Applications. </p>
+    <p><strong>Mrs. Parul Tyagi</strong>-A Taxonomy of Security Attacks and Issues in Vehicular
+ Ad-Hoc Networks (VANETs). </p>
+    <p><strong>Mrs. Parul Tyagi</strong>- Performance Analysis and Quality-of-service Monitoring of
+  Protected and Unprotected TCP Networks using NCTUns Simulator.</p>
+    <p><strong>Mrs. Parul Tyagi</strong>-Investigating the Security Threats in Vehicular ad hoc Networks
+ (VANETs): Towards Security Engineering for Safer on-road Transportation. </p>
+    <p><strong>Mrs. Parul Tyagi</strong>- Performance analysis and implementation of proposed mechanism
+  fordetection and prevention of security attacks in
+  routing protocols of vehicular ad-hoc network (VANET).</p>
+    <p><strong>Mrs. Parul Tyagi</strong>- A Study on Vehicle to Vehicle (V2V) Communication for Safer on –Road
+  Transportation in Vehicle ad hoc Networks.</p>
+    <p><strong>Mr. Rakesh Kardam</strong>-Design of the H-Shape U-Slotted Microstrip Patch Antenna</p>
+    <p><strong>Mr. Rakesh Kardam</strong>- An Extended Approach for Optimizing Antenna. </p>
+    <p><strong>Mr. Rakesh Kardam</strong>- Wireless Communication using visible light future technology. </p>
+    <p><strong>Mr. Rakesh Kardam</strong>- Wireless Communication using visible light future technology. </p>
+    <p><strong>Mr. Rakesh Kardam</strong>- Advanced Digital Wireless Liquid Level Indicator. </p>
+    <p><strong>Ms. Neha Singh</strong>- A Review: Recent Technological Advancement in the field
+  of optical filter”. </p>
+    <p><strong>Ms. Neha Singh</strong>- Designing of Photonic Crystal Ring Resonator based ADF
+ Filter For ITU-T G.694.2 CWDM Systems.</p>
+    <p><strong>Ms. Shikha Gaur</strong>- High Figure of Merit for Highly Sensitive Refractive Index
+  Sensor Based on Metal-Insulator-Metal Plasmonic Bragg
+  Grating. </p>
+
   </div>
 </section>
