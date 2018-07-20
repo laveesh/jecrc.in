8b7eb55a0b3f59001c0417ce6f6f215bd12dede8
Amrit Srivastava <srivastava.amrit1999@gmail.com>
2018-07-20 09:49:00 +0530

    updates w/o photos (#42)

--- departments/me/modules/labs.php
+++ departments/me/modules/labs.php
@@ -1,1 +1,288 @@
-Labs
+<div class="container">
+		<div class="sectionHeading">Laboratories</div>
+		<!-- 1 -->
+		<section class="section">
+			<div class="sectionHeading">MATERIAL'S TESTING LAB</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel" data-slide-to="1"></li>
+						<li data-target="#events_carausel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12 sectionContent">
+					<ul class="sectionList">
+						<li>IMPACT TESTING M/C (EME ROORKEE)</li>
+                        <li>FATIGUE TESTING M/C</li>
+                        <li>SPRING TESTING M/C</li>
+                        <li>ROCKWELL HARDNESS TESTING & B.H </li>
+                        <li>TORSION TESTING M/C</li>
+                        <li>UNIVERSAL TESTING M/C (40 TONS)</li>
+                    </ul>
+				</div>
+			</div>
+		</section>
+		<!-- 2 -->
+		<section class="section">
+			<div class="sectionHeading">DYNAMICS OF MACHINE / KYNATICS OF MACHINE</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel" data-slide-to="1"></li>
+						<li data-target="#events_carausel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12 sectionContent">
+					<ul class="sectionList">
+						<li>STUDY OF QICK RETURN MECHANISM (1 MODEL)</li>
+                        <li>STUDY OF INVERSION OF BOUBLE SLIDE CHAIN (3 MODEL)</li>
+                        <li>STUDY OF VARIOUS CAM AND FLOWER (5MODELS)</li>
+                        <li>VARIOUS TYPES OF DYNOMERS
+                            <ol>
+                                <li>PRONY BRAKE DY NO</li>
+                                <li>ROPE BRAKE DYNO</li>
+                            </ol>
+                        </li>
+                        <li>BAND BRAKE</li>
+                        <li>INERNAL EXPANDING</li>
+                        <li>CONE CLUTCH</li>
+                        <li>INVERSION OF FOUR BAR MECHANISM</li>
+                        <li>CAM ANALYSIS APPRATUS</li>
+                        <li>DETERMINE COEFFICIENT OF FRICTION</li>
+                        <li>GOVERNOR APPARATUS</li>
+                        <li>JOURNAL BEARING APPARATUS</li>
+                    </ul>
+
+				</div>
+			</div>
+		</section>
+		<!-- 3 -->
+		<section class="section">
+			<div class="sectionHeading">FLUID MECHANICS LAB</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel" data-slide-to="1"></li>
+						<li data-target="#events_carausel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12 sectionContent">
+					<ul class="sectionList">
+						<li>APPARATUS FOR CONDUETING ORIFICE EXPERIMENTS (EMI -ROORKEE)</li>
+                        <li>NOTCH APPARATYS TO CALIBRATE V-NOTCH</li>
+                        <li>PITOT TUBE APPARATUS + PIPE FRICTION APPARATUS</li>
+                        <li>BERNOULIS THEORNS </li>
+                        <li>FLOW MEASURMENT BY VENTURIMETER, ORIFICE METER AND NOZZLE METER</li>
+                        <li>METACENTRIC HEIGHT APPARATYS</li>
+                        <li>PELTON TURBINE MODEL</li>
+                        <li>KAPLON RURBINE MODEL</li>
+                        <li>CENTRIFUGAL PUMP TEST RIG</li>
+                        <li>HYDRAULIC RAN TEST RIG</li>
+                    </ul>
+				</div>
+			</div>
+		</section>
+		<!-- 4 -->
+		<section class="section">
+			<div class="sectionHeading">VIBRATION & NOISE ENGG Lab</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel" data-slide-to="1"></li>
+						<li data-target="#events_carausel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12 sectionContent">
+					<ul class="sectionList">
+						<li>WHIRLING OF SHAFT APPARATUS</li>
+                        <li>VIBRATION LAB APPARATUS (NEELAM ENGG)</li>
+                    </ul>
+				</div>
+			</div>
+		</section>
+		<!-- 5 -->
+		<section class="section">
+			<div class="sectionHeading">HEAT-TRANSFER</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel" data-slide-to="1"></li>
+						<li data-target="#events_carausel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12 sectionContent">
+					<ul class="sectionList">
+						<li>THERMAL CONDUCTIVITY OF METAL ROD</li>
+                        <li>EMISSVITY MEASURING APPRATUS</li>
+                        <li>HEAT TRANSFER THROUGH LAGGED PIPE</li>
+                        <li>HEAT TRANSFER THROUGH LAGGED PIPE</li>
+                        <li>HEAT TRANSFER IN AGITATED VESSEL</li>
+                        <li>HEAT TRANSFER IN FORCED CONVECTION</li>
+                        <li>SNELL & TUBE HEAT EXCHANGER</li>
+                        <li>DOUBLE PIPE HEAT EXCHANGER</li>
+                        <li>DROPWISE/FILMWISE CONDENSTION</li>
+                        <li>THERMAL CONDUCTIVITY OF METAL ROD</li>
+                        <li>THERMAL CONDUCTIVITY TEST RIG (INSULATING POWDER)</li>
+                        <li>STAFAN'S BOLTZMANAS APPARATUS</li>
+                        <li>PINFIN TEST RIG</li>
+                        <li>NATURAL CONVNSION TEST RIG</li>
+                    </ul>
+				</div>
+			</div>
+        </section>
+		<!-- 5 -->
+		<section class="section">
+			<div class="sectionHeading">CENTER OF EXCELLENCE</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-12">
+					<div id="events_carausel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carausel" data-slide-to="0" class="active"></li>
+						<li data-target="#events_carausel" data-slide-to="1"></li>
+						<li data-target="#events_carausel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image1.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image2.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/ece/src/img/Events/Image3.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#events_carausel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#events_carausel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+					</div>
+				</div>
+				<div class="col-md-9 col-sm-12 sectionContent">
+					<ul class="sectionList">
+						<li>ANSYS</li>
+                        <li>AUTO CADD</li>
+                        <li>MATLAB</li>
+                        <li>SOLID WORKS</li>
+                        <li>COMSOL</li>
+                        <li>3D PRINTER</li>
+                    </ul>
+				</div>
+			</div>
+        </section>
+
+</div>
