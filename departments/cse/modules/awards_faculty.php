469392c8885b7f35b4308021e807e9e18c536193
Amrit Srivastava <srivastava.amrit1999@gmail.com>
2018-07-23 13:30:16 +0530

    faculty award update w images (#52)

--- departments/cse/modules/awards_faculty.php
+++ departments/cse/modules/awards_faculty.php
@@ -1,30 +1,220 @@
 <section id="awards_faculty" class="container card section">
 	<div class="sectionHeading">Faculty Awards</div>
 	<div class=" card-body sectionContent">
-	<div class="sectionSubHeading">Faculty Awards and Honours</div>
-	<ol class="sectionList" style="list-style-type: upper-roman;">
-<li><strong><em>Doctorate Awarded to Ms. Jyoti Gautam.</em></strong>The degree of Doctor of Philosophy was conferred to Ms. Jyoti Gautam on 12th April, 2016 by Gautam Buddha University (Established by UP Act No. 9 of 2002) based on the recommendations of the academic council of the university.</li>
-<li><strong><em>Selection in PhD and Other Technical Qualification :</em></strong>
-<ol class="sectionList" style="list-style-type: upper-roman;">
-<li>Mr. Rohit Agarwal selected for PhD program in IIT, Indore.</li>
-<li>Mr. Mahboob Alam selected for PhD program in Jamia Milia Islamia, Delhi.</li>
-<li>Mr. Sur Singh Rawat selected for PhD program in G.B.Pant Engineering College, Garhwal, Uttarakhand.</li>
-<li>Mr. Krishnanand Chaturvedi qualified JEST 2015 (Computer Science) conducted by IIMSc, Chennai.</li>
-<li>Mr. Krishnanand Chaturvedi ,Mr. Vimal Gupta and Mrs. Jaspreet Kaur selected by UPTU for PhD program in CSE.</li>
-</ol>
-</li>
-<li><strong><em>Ajay Kumar Verma attended:</em></strong>
-<ol class="sectionList" style="list-style-type: upper-roman;">
-<li>One Week Short term Course (FDP) by NITTTR Chandigarh on "MATLAB and its Hardware Interface" from 11-01-16 to 15-01-16 at JSSATE, Noida.</li>
-<li>Survey on the Methodologies to Improve the Scalability of SWS in International Journal of Advance Research in Engineering Science and Technology, ISSN (Online): 2393-9877, ISSN (print): 2394-2444, Volume 03, Issue 02,IF: 3.63,February-2016</li>
-<li>Participated in Two days workshop on<strong>Big Data  Hadoop</strong>conducted by APPFLUXUS 2016 in collaboration with EDP Cell, IIT Roorkee from 4th November - 5th November, 2015.</li>
-</ol>
-</li>
-<li>Ajay Kumar Verma and Ms. Gunjan Goswami attended a One Week Short term Course (FDP) on "Operation Research"conducted by NITTTR Chandigarh from 28-03-16 to 01-04-16 at JSSATE, Noida</li>
-<li>Ms. Urvashi Rahul Saxena, Ms. Jaspreet Kaur, Ajay Kumar Verma and Ms. Suruchi Saberwal attended a one week FDP on Students Evaluation from 16th Nov-15 to 20th Nov-15 by NITTTR Chandigarh conducted at JSSATE, Noida</li>
-<li>Ms. Urvashi Rahul Saxena and Ms. Suruchi Saberwal attended a two- week FDP on Research Methodology from 28th Dec-15 to 8th Jan-16 by NITTTR Chandigarh conducted at JSSATE, Noida.</li>
-<li>Ms. Bhavna Bansal and Mr. Girish Kumar B.C. participated in STC on ICT Based programme on Sustainable Environmental Management conducted on 14-18 March, 2016.</li>
-<li>Ms.Deepti Aggarwal, Ms. Sonali Mathur, Ms. Anita Sahoo attended ICT Based FDP on Effective Curriculum Implementation from 7th to 11th March, 2016 in collaboration with NITTTR Chandigarh in Seminar Hall in JSSATE, Noida.</li>
-</ol>
-</div>
+	<div class="container text-justify">
+	  <h2>Recent achievements of Faculties</h2>
+	  <div class="card mt-2">
+	    <div class="card-header">Prof. (Dr.) Vijay Singh Rathore</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/1.png" height="300px" width="200px">
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+	    				<li>
+		    			Honorary Appreciation cum Felicitation Ceremony 2017 ( By
+						G R Foundation ) Supported By – CSI Division IV, COEP Pune, in
+						concurrent with Inaugural Function of SMARTCOM 2017 in
+						recognition of the services to the Society at Large in different
+						sector including the Academic Sector.
+						<li>
+						Certificate of Appreciation For Immense Support and
+						contribution towards making the “Indian Higher Education
+						Knowledge Delegation to UK” highly successful and beneficial,
+						By Prof Anil D. Sahashtrabudhe, Chairman, AICTE (India).
+						<li>
+						Award of Prominent Academician by Association of Computing
+						Machinery (ACM).
+					</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	  <div class="card mt-2">
+	    <div class="card-header">Dr. Bhavna Sharma</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/2.jpg" height="300px" width="200px">
+
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+	    				<li>
+	    				Received Distinguished Service Award by JECRC Foundation for
+						Organizer and Coordinator for Smart India Hackathon-2017.
+						<li>
+						Organizer for Smart India Hackathon 2018 for JECRC as Nodal
+						Centre.
+						<li>
+						Co-convener, International Conference on Emerging Trends in
+						Expert Application &amp; Security, Feb. 17-18, 2018.
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	  <div class="card mt-2">
+	    <div class="card-header">Mr. Mukesh Agarwal</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/3.png" height="300px" width="200px">
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+	    				<li>Completed M. Tech in Software Engineering from BITS-Pilani.</li>
+						<li>Published research paper in Springer AISC (ISSN No. 2194-5357)
+						in International Congress of Information and Communication
+						Technology titled, ‘A Comparative Study of Mindfulness between
+						Meditators and Non-meditators’ .</li>
+						<li>Applied Projects worth Rs. 5371 Lacs to Govt of India in 2017-18.
+						<li>Organized International Training Programme on Entrepreneurship
+						Education to Strengthen Emerging Economies and Sustainable
+						Livelihoods And Mainstreaming with Market (7th February 2018)
+						under the aegis of ITEC-MEA, Govt. of India, New Delhi and
+						hosted students from 26 Countries.</li>
+						<li>Received Rural Technology Business Incubator with grant of Rs.
+						30 lacs by DST Rajasthan in March 2018.</li>
+						<li>Felicitated by Prof. Anil D Sahasrabuddhe, Chairman AICTE on
+						Foundation Day for establishing unique Spiritual Research Cell at
+						JECRC.</li>
+						<li>Invited by AICTE for Survey of Health parameters of AICTE
+						Executives in a Vice Chancellors Meet.</li>
+						<li>Invited to organize Yoga Day at JECRC University.</li>
+						<li>Conducted sessions of Self-Empowerment through meditation at
+						Spiritual Research Cell for First year students.</li>
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	  <div class="card mt-2">
+	    <div class="card-header">Mr. Gajendra Sharma</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/4.png" height="300px" width="200px">
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+	    				<li>Organized Vande Mataram event at SMS, Stadium , Jaipur .
+						<li>Faculty Coordinator- “Big Data” workshop conducted in
+						collaboration with IIT, Kharagpur.
+						<li>Participated in ICARI-2018 held at institution of Engineers, Delhi
+						on 28 th Jan 2018.
+						<li>Coordinator – PMKVY, Ted visits.
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	  <div class="card mt-2">
+	    <div class="card-header">Dr. Sanjay Gaur</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/5.jpg" height="300px" width="200px">
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+						<li>Received Achiever of Year 2017 award for Significant
+						Contribution in Computer Science and Engineering by The
+						Institution of Engineers –Udaipur Chapter on Engineers’ Day.
+
+						<li>Received Global Outreach Education Excellence Award 2018
+						for Significant Contribution in Computer Science &amp; Information
+						Technology by REDINNO–Jaipur.
+						<li>Delivered Seminar/ Expert Talk on Digital Marketing in following
+						Institutions:
+						<li>JRN Rajasthan Vidyapeeth Udaipur, 27-18 November 2017.
+						<li>Directorate of Extension Education, Maharana Pratap
+						University of Agriculture and Technology-Udaipur 16-22,
+						January 2018.
+						<li>Vivekanand Global University, Jaipur , 30 March 2018.
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	  <div class="card mt-2">
+	    <div class="card-header">Dr. Nilam Choudhary</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/6.png" height="300px" width="200px">
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+						<li>QEEE Program Coordinator of JECRC Foundation, attended
+						QEEE Feedback Meeting Workshop IIT Chennai.
+						<li>Chaired the session in International Conference on Engineering,
+						Computers and Sciences (IRECS-2018).
+						<li>Delivered Invited Talk on “Internet of Things Implementation
+						Issues and Challenges” in Global Outreach Education Conference
+						and Awards by REDINNO.
+						<li>Delivered Invited Talk on “Cloud computing implementation tools
+						and issues” in National Conference of Computational and
+						Mathematical Science by Vivekananda Global, Jaipur
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	  <div class="card mt-2">
+	    <div class="card-header">Ms. Manju Vyas</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/7.jpg" height="300px" width="200px">
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+						<li>Nodal Coordinator for Smart India Hackathon 2018 at JECRC,
+							Jaipur
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	  <div class="card mt-2">
+	    <div class="card-header">Ms. Shikha Maheshwari</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/8.jpg" height="300px" width="200px">
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+						<li>Recognized as Microsoft Innovation Educator-Expert (MIE-E)
+						2017-18.
+						<li>Appreciated for Services &amp; Performance by JECRC Foundation,
+						September 2017.
+						<li>Co-convener, International Conference on Emerging Trends in
+						Expert Application &amp; Security (ICETEAS), Feb. 17-18, 2018 .
+						<li>Organized various International Seminar/ Workshops as Co-
+						Convener/ Coordinator.
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	  <div class="card mt-2">
+	    <div class="card-header">Mr. Abhishek Dixit</div>
+	    <div class="card-body">
+	    	<div class="row">
+	    		<div class="col-md-3 border-right">
+	    			<img src="../departments/cse/src/img/awards/faculties/9.jpg" height="300px" width="200px">
+	    		</div>
+	    		<div class="col-md-9">
+	    			<ul class="sectionList px-4">
+						<li>Certificate of Appreciation for contribution in growth of students
+							as Training and Placement officer.
+	    			</ul>
+	    		</div>
+	    	</div>
+	    </div>
+	  </div>
+	</div>
 </section>
