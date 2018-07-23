af481baf8a760af9a29c665ad4702ce6230c7cc6
Arpit jain <maiarpitjain94@gmail.com>
2018-07-23 11:57:54 +0530

    me alumni updated (#50)
    * me alumni updated
    
    * update me staff pics
    
    * update alumni pics
    
    * corousel images
    
    * add publication pdf

---
+++ departments/ece/modules/publication.php
@@ -0,0 +1,8 @@
+<!-- research publication page -->
+<section class="section container card">
+  <div class="sectionHeading">Publications</div>
+  <div class="sectionContent card-body px-4">
+<iframe src="http://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/me/src/pdf/books_database.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>   
+
+  </div>
+</section>
