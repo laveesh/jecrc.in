5928eda3f1884d0e2d7379bd1aadf7d767ba854f
Arpit jain <arpitkumarjain.cse20@jecrc.ac.in>
2018-07-23 07:20:14 +0530

    tf

--- departments/ece/modules/publication.php
+++
@@ -1,8 +0,0 @@
-<!-- research publication page -->
-<section class="section container card">
-  <div class="sectionHeading">Publications</div>
-  <div class="sectionContent card-body px-4">
-<iframe src="http://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/ece/src/pdf/books_database.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>   
-
-  </div>
-</section>
