ceb4356eb1ec04e6467aeca707f5698bc1440cd9
Arpit jain <maiarpitjain94@gmail.com>
2018-07-25 09:41:08 +0530

    ee data update (#59)
    * Merge branch 'master' into arpit-patch-1
    
    * faculty_awards , student_awards,co_po_maping,Course_outcome,Library

--- departments/ee/modules/awards_student.php
+++ departments/ee/modules/awards_student.php
@@ -5,13 +5,13 @@
 		<div class="sectionSubHeading">Students Awards and Honours </div>
 
 		<ol class="sectionList group-item group-item-flush">
-			<li class="list-group-item">Rohit Raj from ECE branch (2nd year) has been selected as a scholar in Google India Challenge Scholarship 2018 by Udacity on 7th February 2018</li>
-			<li class="list-group-item">Kanishka Kukreja of ECE department (final year) received a Accenture women Achiever award in year 2017 on women's day celebration at college campus by Accenture.</li>
-			<li class="list-group-item">Ms. Rashi Bansal of ECE department (Second year) won the event “PARIDHAN” of National Tech Management Fest held at JK Lakshmipat University, Jaipur.
+			<li class="list-group-item">Mr. Sudesh Choudhary (2nd Year) has secured 2nd rank in Volleyball match of JU Verve 2018.
 </li>
-			<li class="list-group-item">Ms. Rashi Bansal of ECE department (Second year) secured first position in the event “PANACHE” conducted by Creative Arts and Cultural Society held at MNIT, Jaipur.
+			<li class="list-group-item">Student Kapil Bindal (4 th year) got selected in CAT 2018 exam scoring 95.16 percentile .He has been shortlisted for admission to: IIM Bhodhgaya IIM Kashipur IIM Raipur IIM Ranchi IIM Rohtak IIM sambalpur IIM Sirmaur IIM Udaipur IIM Tiruchirappalli</li>
 
-</li>
+			<li class="list-group-item">Student Prakhar Joshi (3 rd year) won Best Delegate prize in INTRA-MUN JECRC 2018 held at JECRC University.</li>
+			
+			
 		</ol>
 		
 
--- departments/ee/modules/course_outcomes.php
+++ departments/ee/modules/course_outcomes.php
@@ -1,1 +1,4 @@
-cours outcomes
+<section class="section container card">
+	<div class="sectionHeading">COURSE OUTCOME</div>
+	<iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/ee/src/pdf/course_outcome.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>		
+</section>
