76249d2382b0be639b99f7de96111b7442f62fc8
amritsrivastava <srivastava.amrit1999@gmail.com>
2018-07-25 17:55:41 +0530

    improvments

--- departments/me/modules/awards_faculty.php
+++ departments/me/modules/awards_faculty.php
@@ -6,34 +6,44 @@
 		<li><strong><em>Dr. M.P Singh</em></strong>
 			<ul class="sectionList">
 				<li>Selected for <strong>Kirchhoff Research Award-2015</strong> in Mechanical Engineering  by <strong>IASR</strong></li>
-				<li> Session chair in <strong>International conference</strong> held at SKIT Jaipur in 2016</li> 
-				<li> Author of four books</li> 
-				<li> Number of  publications in reputed  Journals</li> 
+				<li>Session chair in <strong>National /International conferences.</strong></li>
 			</ul>
 		</li>
-		<li><strong><em>Manish Jain</em></strong>
-				<p>Session chair  in International conference held at SKIT Jaipur in 2016</p>
+		<li><strong><em>Mr. Manish Jain</em></strong>
+			<ul class="sectionList">
+				<li>Session chair in <strong>National /International conferences</strong></li>
+				<li>Visited Italy as faculty advisor of JECRC SAE team (MOONRIDERS) participated in <strong>FORMULA SAE ITALY 2011</strong></li>
+			</ul>
 		</li>
 		<li><strong><em>Dr. Bhuvnesh Bhardwaj</em></strong>
 			<ul class="sectionList">
 				<li>Obtained patent in Apparatus and method for cleaning inner surface of hollow object <strong>(Application no. 4307/DEL/2015, Publication date: 30/06/17).</strong></li>
-				<li>Providing consultancy</li>
-				<li>Number of  publications in reputed  <strong>Journals (Thomson Reuters)</strong> and citation as well.</li>
-				<li>Session chair in <strong>International conference</strong> held at SKIT Jaipur in 2016</li>
-				<li>Session chair in <strong>National conference</strong> held at <strong>REC Jaipur</strong> in 2017</li>
+				<li>Providing consultancy to different Industries.</li>
+				<li>Session chair in <strong>National /International conferences</strong></li>
+				<li>Expert talks in different FDP’s.</li>
 			</ul>
 		</li>
 		<li><strong><em>Dr. Manish Srivastava</em></strong>
 			<ul class="sectionList">
-				<li>One student submitted her PhD. In the year 2018</li>
 				<li>Received appreciation letter in Hackathon 2017 by principal JECRC.</li>
 				<li>Expert talks on <strong>“E-Mobility” at IIT-Delhi</strong> in July 2017</li>
-				<li>Author of 2 books.</li>
 			</ul>
 		</li>
-		<li><strong><em>Kuldeep Sharma</em></strong>
+		<li><strong><em>Mr. Kuldeep Sharma</em></strong>
 			<ul class="sectionList">
 				<li>Appreciation received from <strong>department of IMMUNO Hematology and transfusion</strong>, SMS medical college regarding commonable work for organizing mega blood donation camp in 2017.</li>
+				<li>Senior advisor of <strong>JECRC SAE Team (Moonriders)</strong></li>
+			</ul>
+		</li>
+		<li><strong><em>Mr. Satyendra Kumar</em></strong>
+			<ul class="sectionList">
+				<li>Expert talks in different FDP’s.</li>
+				<li>Faculty advisor of <strong>JECRC SAE Team (Moonriders)</strong></li>
+			</ul>
+		</li>
+		<li><strong><em>Mr. Ravi Yadav</em></strong>
+			<ul class="sectionList">
+				<li>Delivered Workshops on <strong>“Common Diagnosis of Automotive Vehicles”</strong> under <strong>“Skill India Mission”, Government of India</strong>.</li>
 			</ul>
 		</li>
 	</ol>
