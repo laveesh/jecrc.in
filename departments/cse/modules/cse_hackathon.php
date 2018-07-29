eec0a78f0c248d055be4a780578423a157954c6c
aditya81070 <aditya81070@gmail.com>
2018-07-29 12:13:05 +0530

    Add Hackathon detail

---
+++ departments/cse/modules/cse_hackathon.php
@@ -0,0 +1,66 @@
+<section class="section container">
+	<div class="sectionHeading">CSE Hackthon</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div id="csehack_carausel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#csehack_carausel" data-slide-to="0" class="active"></li>
+						<li data-target="#csehack_carausel" data-slide-to="1"></li>
+						<li data-target="#csehack_carausel" data-slide-to="2"></li>
+						<li data-target="#csehack_carausel" data-slide-to="3"></li>
+					</ol>
+					<div class="carousel-inner ">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="../departments/cse/src/img/Events/csehack/img1.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/cse/src/img/Events/csehack/img2.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/cse/src/img/Events/csehack/img3.jpg" alt="Third slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="../departments/cse/src/img/Events/csehack/img4.jpg" alt="Fourth slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#csehack_carausel" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#csehack_carausel" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+		</div>
+		<div class="col-md-6">
+			<p class="px-3 pt-3">JECRC Foundation has emerged as one of the top ranking engineering
+colleges within a short span of time. It has always taken various
+initiatives for the students to deepen their insight, widen their horizon
+and create a meaningful outlook. In the series of taking such wonderful
+initiatives, Shri Arpit Agarwal, Director, JECRC Foundation launched
+JECRC HACKATHON 1.0 so that a coding environment can be
+provided to the budding engineers of the college.</p>
+<p class="px-3 pb-3">
+	JECRC Foundation has emerged as one of the top ranking engineering
+colleges within a short span of time. It has always taken various
+initiatives for the students to deepen their insight, widen their horizon
+and create a meaningful outlook. In the series of taking such wonderful
+initiatives, Shri Arpit Agarwal, Director, JECRC Foundation launched
+JECRC HACKATHON 1.0 so that a coding environment can be
+provided to the budding engineers of the college.
+</p>
+		</div>
+	</div>
+	<div class="sectionContent">	
+	<p class="px-3">The problems were taken from various ministries, departments as well as from JECRC Foundation itself and the ideas of the students were finalized after going through two rigorous scrutiny tests. Out of 300 teams registered, 152 teams were finally selected and 768 students converted their ideas into new developments.</p>
+	<p class="px-3">The inauguration ceremony of this event was graced by the presence of Prof. Dileep N. Malkhede, Advisor, AICTE and Prof. N.P. Kaushik, Vice Chancellor, RTU, Kota. Also, we were honoured to have Mr. Mohammad Imran Khan, Web developer and Ms. Nayani Nasa, Startup Manager, Invest India with us.</p>
+	<p class="px-3">The coding session started and 12 judges along with 123 mentors fertilized the raw brains of the students with their experiences. Each judge was allotted 14 teams to evaluate. After 2 rounds of judging, finally 47 teams were ready for the power judging round.</p>
+	<p class="px-3">Meanwhile, some recreational activities were also organised to regenerate the energy and enthusiasm of the students. In the mid night of Jan 10th, 2018, a huge bonfire was lit in the central lawn of JECRC Campus. It created a soothing night time ambience and the zeal returned in everyone to work throughout the night.</p>
+	<p class="px-3">The valedictory session was organised on Jan 11th, 2018 and was graced by the presence of Dr. Ravi Kumar Maddila, Assistant Professor, Department of ECE, MNIT; Dr. Sanjeev Agarwal, Associate Professor, Department of ECE, MNIT and Dr. Ashish Kr. Ghunawat, Assistant Professor, Department of ECE, MNIT.</p>
+	<p class="px-3">In the valedictory session, the top 21 teams were awarded by our eminent academicians. The team "The Ultimates" secured the first position and awarded with cash prize of Rs. 11,000. Also, a prize of Rs. 5100 was given to a girls’ team to encourage women empowerment in the society.</p>
+	<p class="px-3">Also, to appreciate the hard work of the mentors, Dr. Ruchi Mathur, Mr. Naveen Kedia and Dr. Bhavna Sharma were selected as top three mentors by the judges. The sponsors of JECRC HACKATHON 1.0 were Engineer's Academy, Forsk Technologies and Road Ahead Technologies India Pvt. Ltd.</p>
+</div>
+
+
+</section>
