09755621cdfdf4395ce7794d159e8c7958abe969
aditya81070 <aditya81070@gmail.com>
2018-07-29 12:12:14 +0530

    Add cse contact

--- departments/cse/modules/contact.php
+++ departments/cse/modules/contact.php
@@ -1,10 +1,10 @@
 <section class="section container card">
 	<div class="sectionHeading">Contact Us</div>
 	<div class="sectionContent card-body">
-		<p>To request additional information concerning the Department of Computer Science and Engineering, you can reach us at: <a href="mailto:info.cse@jssaten.ac.in">info.cse@jssaten.ac.in</a></p>
+		<p>To request additional information concerning the Department of Computer Science and Engineering, you can reach us at:</p>
 		<h4 class="sectionSubHeading">CSE Head</h4>
-		<p>AB-I First Floor</p>
-<p>Phone:(0120)2400104, Ext:  241 </p>
-<p><a href="mailto:hodcse@jssaten.ac.in">hodcse@jssaten.ac.in</a></p>
+		<p>A-Block II Floor EDC Conference Room</p>
+<p>Phone:+91-9251039865</p>
+<p><a href="mailto:hod.cse@jecrc.ac.in">hod.cse@jecrc.ac.in</a></p>
 	</div>
 </section>
