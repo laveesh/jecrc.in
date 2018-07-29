e89befb611d08e477137cf599d3651dcc8a8b576
aditya81070 <aditya81070@gmail.com>
2018-07-29 12:55:45 +0530

    Add container

--- departments/cse/modules/co_po_mapping.php
+++ departments/cse/modules/co_po_mapping.php
@@ -1,4 +1,4 @@
-<section class="section containter">
+<section class="section container">
 	<div class="sectionHeading">CO-PO Mapping</div>
 	<iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/cse/src/pdf/copomapping.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>
 </section>
