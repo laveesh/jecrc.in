3d2ea24b9ac508f044cfe7091128183e55b53f67
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-08-01 11:48:43 +0530

    w/o images, upto 5sem

--- departments/civil/modules/course_outcomes.php
+++ departments/civil/modules/course_outcomes.php
@@ -27,89 +27,231 @@
 					
 			
 					<tr>
-							<th scope="row">2</th>
-							<td>3CE2A</td>
-							<td>Civil Engineering Materials </td>
+						<th scope="row">2</th>
+						<td>3CE2A</td>
+						<td>Civil Engineering Materials </td>
+						<td>
+							<ul class="list-group list-group-flush">
+								<li class="list-group-item">CO1: Understanding the basic properties classification & uses of stone</li>
+								<li class="list-group-item">CO2: Understanding the basic properties manufacturing & uses of clay product, cement & lime</li>
+								<li class="list-group-item">CO3: Understanding the function, various test & application of mortar and plaster timber & steel</li>
+								<li class="list-group-item">CO4: Study & analyzing the concept of environmental friendly building material and miscellaneous materials such as glass, aluminum, asbestos, G.I., plastics in construction</li>
+							</ul>
+						</td>
+					</tr>
+						<tr>
+							<th scope="row">3</th>
+							<td>3CE3A</td>
+							<td>Engineering Geology </td>
 							<td>
 								<ul class="list-group list-group-flush">
-									<li class="list-group-item">CO1: Understanding the basic properties classification & uses of stone</li>
-									<li class="list-group-item">CO2: Understanding the basic properties manufacturing & uses of clay product, cement & lime</li>
-									<li class="list-group-item">CO3: Understanding the function, various test & application of mortar and plaster timber & steel</li>
-									<li class="list-group-item">CO4: Study & analyzing the concept of environmental friendly building material and miscellaneous materials such as glass, aluminum, asbestos, G.I., plastics in construction</li>
+									<li class="list-group-item">CO1: Understands the concepts of general geology & knows about internal structure of earth & minerals.</li>
+									<li class="list-group-item">CO2: Understanding the concepts of petrology & know about rocks</li>
+									<li class="list-group-item">CO3: Understanding the concept of structural geology & basics of fold & faults & also to know about dip & strike problems.</li>
+									<li class="list-group-item">CO4: Basics of geophysical methods for the subsurface analysis of earth and concept of remote sensing & GIS & their applications in civil engineering</li>
 								</ul>
 							</td>
 						</tr>
-						<tr>
-								<th scope="row">3</th>
-								<td>3CE3A</td>
-								<td>Engineering Geology </td>
+
+							<tr>
+								<th scope="row">4</th>
+								<td>3CE4A</td>
+								<td>Construction Technology</td>
 								<td>
 									<ul class="list-group list-group-flush">
-										<li class="list-group-item">CO1: Understands the concepts of general geology & knows about internal structure of earth & minerals.</li>
-										<li class="list-group-item">CO2: Understanding the concepts of petrology & know about rocks</li>
-										<li class="list-group-item">CO3: Understanding the concept of structural geology & basics of fold & faults & also to know about dip & strike problems.</li>
-										<li class="list-group-item">CO4: Basics of geophysical methods for the subsurface analysis of earth and concept of remote sensing & GIS & their applications in civil engineering</li>
+										<li class="list-group-item">CO1: To Understand The Basic Building Requirement And Construction Techniques.</li>
+										<li class="list-group-item">CO2: To Understand About DPC Joints Arches Lintels Etc.</li>
+										<li class="list-group-item">CO3: To Understand the Fabrication And Erection Work, Roof And Roof Covering</li>
+										<li class="list-group-item">CO4: To Understand Advanced Construction Techniques and About Equipment Management</li>
 									</ul>
 								</td>
 							</tr>
 
-							<tr>
-									<th scope="row">4</th>
-									<td>3CE4A</td>
-									<td>Construction Technology</td>
+								<tr>
+									<th scope="row">5</th>
+									<td>3CE5A</td>
+									<td>Fluid Mechanics</td>
 									<td>
 										<ul class="list-group list-group-flush">
-											<li class="list-group-item">CO1: To Understand The Basic Building Requirement And Construction Techniques.</li>
-											<li class="list-group-item">CO2: To Understand About DPC Joints Arches Lintels Etc.</li>
-											<li class="list-group-item">CO3: To Understand the Fabrication And Erection Work, Roof And Roof Covering</li>
-											<li class="list-group-item">CO4: To Understand Advanced Construction Techniques and About Equipment Management</li>
+											<li class="list-group-item">CO1: To understand important basic terms used in fluid mechanics with practices of solvig problem</li>
+											<li class="list-group-item">CO2: To understand hydrostatics and buoyancy with practice of solving problem</li>
+											<li class="list-group-item">CO3: To understand euler equation, bernoulli equation in hydro kinematics, rotational flow practice of applications and solving problems.</li>
+											<li class="list-group-item">CO4: To understand momentum equation and flow throw pipes practice of solving problems.</li>
 										</ul>
 									</td>
 								</tr>
 
-								<tr>
-										<th scope="row">5</th>
-										<td>3CE5A</td>
-										<td>Fluid Mechanics</td>
+									<tr>
+										<th scope="row">6</th>
+										<td>3CE6A</td>
+										<td>Advanced Engineering Mathematics</td>
 										<td>
 											<ul class="list-group list-group-flush">
-												<li class="list-group-item">CO1: To understand important basic terms used in fluid mechanics with practices of solvig problem</li>
-												<li class="list-group-item">CO2: To understand hydrostatics and buoyancy with practice of solving problem</li>
-												<li class="list-group-item">CO3: To understand euler equation, bernoulli equation in hydro kinematics, rotational flow practice of applications and solving problems.</li>
-												<li class="list-group-item">CO4: To understand momentum equation and flow throw pipes practice of solving problems.</li>
+												<li class="list-group-item">CO1: Calculate the Fourier series for standard periodic waveforms and demonstrate their understanding of the Dirichlet’s conditions by using them to evaluate infinite series. solve z transform and its difference equations</li>
+												<li class="list-group-item">CO2:  Calculate the Laplace transform of standard functions and use the techniques to solve second-order ordinary differential equations and partial differential equations.</li>
+												<li class="list-group-item">CO3: Calculate the Fourier transform of standard functions and use the techniques to solve second-order ordinary differential equations and partial differential equations.</li>
+												<li class="list-group-item">CO4: Use numerical methods to interpolate, differentiate and integrate the functions and solve first order differential equations</li>
 											</ul>
 										</td>
 									</tr>
-
-									<tr>
-											<th scope="row">6</th>
-											<td>3CE6A</td>
-											<td>Advanced Engineering Mathematics</td>
+<!-- sem4 -->
+										<tr>
+											<th scope="row">7</th>
+											<td>4CE1A</td>
+											<td>Strength of Materials-II</td>
 											<td>
 												<ul class="list-group list-group-flush">
-													<li class="list-group-item">CO1: Calculate the Fourier series for standard periodic waveforms and demonstrate their understanding of the Dirichlet’s conditions by using them to evaluate infinite series. solve z transform and its difference equations</li>
-													<li class="list-group-item">CO2:  Calculate the Laplace transform of standard functions and use the techniques to solve second-order ordinary differential equations and partial differential equations.</li>
-													<li class="list-group-item">CO3: Calculate the Fourier transform of standard functions and use the techniques to solve second-order ordinary differential equations and partial differential equations.</li>
-													<li class="list-group-item">CO4: Use numerical methods to interpolate, differentiate and integrate the functions and solve first order differential equations</li>
+													<li class="list-group-item">CO1: Study the deflection of Beams using following methods: double integration method, Macaulay’s method, area moment method and conjugate beam method.</li>
+													<li class="list-group-item">CO2:  Analysis of prop cantilever structures, determinate Structure using Area moment method, Conjugate beam method and Fixed Beams & Continuous Beams by three moments Theorem and Area moment method. Study of combined direct and bending stress, middle third rule, core of a section, gravity retaining wall.</li>
+													<li class="list-group-item">CO3: Study of Torsion, shear stress in solid and hollow circular shafts, angle of twist, power transmitted by a shaft, combined bending and torsion. Study of following points about Springs: Stiffness of springs, springs in series and parallel, laminated plate springs, leaf spring, close coiled helical springs, open coiled springs. Vibrations.</li>
+													<li class="list-group-item">CO4: Study of Simple Harmonic Motion and Introduction to damped and forced vibration. Acquire knowledge about Undamped free vibration of SDOF system: Newton’s law of motion, D’Almbert’s principle.</li>
 												</ul>
 											</td>
 										</tr>
-<!-- sem4 -->
 										<tr>
-												<th scope="row">6</th>
-												<td>3CE6A</td>
-												<td>Advanced Engineering Mathematics</td>
-												<td>
-													<ul class="list-group list-group-flush">
-														<li class="list-group-item">CO1: Calculate the Fourier series for standard periodic waveforms and demonstrate their understanding of the Dirichlet’s conditions by using them to evaluate infinite series. solve z transform and its difference equations</li>
-														<li class="list-group-item">CO2:  Calculate the Laplace transform of standard functions and use the techniques to solve second-order ordinary differential equations and partial differential equations.</li>
-														<li class="list-group-item">CO3: Calculate the Fourier transform of standard functions and use the techniques to solve second-order ordinary differential equations and partial differential equations.</li>
-														<li class="list-group-item">CO4: Use numerical methods to interpolate, differentiate and integrate the functions and solve first order differential equations</li>
-													</ul>
-												</td>
-											</tr>
-								
+											<th scope="row">8</th>
+											<td>4CE2A</td>
+											<td>CONCRETE TECHNOLOGY</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: to understand ingredient of concrete and its properties.</li>
+													<li class="list-group-item">CO2: to understand different types of concrete and non destructive tests.</li>
+													<li class="list-group-item">CO3: to understand the concrete handling in fields and concrete mix design..</li>
+													<li class="list-group-item">CO4: understand admixtures, formworks and type of concrete.</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">9</th>
+											<td>4CE3A</td>
+											<td>HYDRAULICS & HYDRAULIC MACHINE</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: to understand dimension analysis & models.</li>
+													<li class="list-group-item">CO2:  to understand laminar flow & turbulent flow.</li>
+													<li class="list-group-item">CO3: to understand open channel flow.</li>
+													<li class="list-group-item">CO4: understand impact of jets , pumps and turbines.</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">10</th>
+											<td>4CE4A</td>
+											<td>Surveying-I</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: Study the Introduction to surveying and its Importance to engineers, Plane and geodetic surveying, Basic principle of surveying from whole to part and conventional signs. Study how to measure Distances.</li>
+													<li class="list-group-item">CO2: To know about the Measurement of Angles & Direction, Bearing andazimuths, magnetic declination and its variation. To know how to use the following instruments:surveyors and prismatic compass. Vernier and micro-optic theodolite, </li>
+													<li class="list-group-item">CO3: Study of different methods of traversing: chain traverse & Compass traverse, transit-tape traverse.Methods of computations and adjustment of traverse; transit rule, Bowditch rule, graphical method, axis method. Gales traverse table.</li>
+													<li class="list-group-item">CO4: To provide basic knowledge about Leveling and Plane Table Surveying.</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">11</th>
+											<td>4CE5A</td>
+											<td>BUILDING PLANNING</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: to understand types of buildings & appropriate selection of site with sun consideration.</li>
+													<li class="list-group-item">CO2: to study about bye-laws and nbc regulation along with orientation, climatic & comfort consideration.</li>
+													<li class="list-group-item">CO3: understand principles of planning and vastushastra.</li>
+													<li class="list-group-item">CO4: to understand functional design and services in buildings.</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">12</th>
+											<td>4CE6A</td>
+											<td>QUANTITY SUVEYING & VALUATION</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: To understand about the Estimate. Their type, method, and preparing cost of civil works.</li>
+													<li class="list-group-item">CO2: To understand about the analysis of rates of various items of civil   engineering works and how to prepare</li>
+													<li class="list-group-item">CO3: TO understand about  over head chargescontingencies and w.c. Establishment estimate of different works.</li>
+													<li class="list-group-item">CO4: To understand about the depreciation, sinking fund, valuation of civil works, rent calculation of govt building etc.</li>
+												</ul>
+											</td>
+										</tr>
+<!-- sem 5 -->
+										<tr>
+											<th scope="row">12</th>
+											<td>5CE1A</td>
+											<td>Theory of Structures-I</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: Understand the indeterminacy of structures & can apply the concepts of Maxwell & BETTE theorem & Slope deflection theorem</li>
+													<li class="list-group-item">CO2: Understanding the concepts of Moment distribution method</li>
+													<li class="list-group-item">CO3: Understanding the concept of Strain energy & application of Castiglione theorems & Unit load method to find the deformations of structures</li>
+													<li class="list-group-item">CO4: Basic knowledge of Column analogy method & KANI method & analyze the multistory frames using portal method, factor method, cantilever method & analysis of determinate trusses by Tension coefficient method.</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">13</th>
+											<td>5CE2A</td>
+											<td>Environment Engineering-I</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: To understand the basics of environmental science and engineering and importance of water.</li>
+													<li class="list-group-item">CO2: To understand the role of civil engineer in environment protection.</li>
+													<li class="list-group-item">CO3: To learn various terms of WSS.</li>
+													<li class="list-group-item">CO4: To understand various sources, quality and standards of water.</li>
+													<li class="list-group-item">CO5: To understand methods, components and designing of water distribution system.</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">14</th>
+											<td>5CE3A</td>
+											<td>Geotechnical Engineering</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: To Understand basic properties of soil and their identifications.</li>
+													<li class="list-group-item">CO2: To Understand behavior of clay mineralogy</li>
+													<li class="list-group-item">CO3: TO understand the Stresses in soil mass and Seepage and Seepage Pressure.</li>
+													<li class="list-group-item">CO4: Basic knowledge of Mohr’s circle of stress and Principles of soil compaction</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">15</th>
+											<td>5CE4A</td>
+											<td>Surveying-II</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: Basic understanding of survey trigonometric leveling for various cases, various correction    including effect of earth curvature and refraction</li>
+													<li class="list-group-item">CO2: Understanding the basic implementation of horizontal curve on ground for all cases whether centre and radius are accessible or not.</li>
+													<li class="list-group-item">CO3: Understand how adjustment of figure for surveying of any country and all routine work to draw a triangulation system and various types of errors.</li>
+													<li class="list-group-item">CO4: Study of astronomical observation based on earth and celestial sphere, and co-ordinate system of earth</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">16</th>
+											<td>5CE5A</td>
+											<td>Building Design</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: this subject will provide the understanding of different types of loads considered and taken into account when designing the buildings especially DL/LL/IL/EL.</li>
+													<li class="list-group-item">CO2: Students will be able to understand the different types of construction and various provisions specified by bureau of Indian standards.</li>
+													<li class="list-group-item">CO3: Students will be able to understand the design criteria of the buildings/ roof (general &special)/ building components and the modern techniques involved in construction</li>
+												</ul>
+											</td>
+										</tr>
+										<tr>
+											<th scope="row">16</th>
+											<td>5CE6.3A</td>
+											<td>Solid Waste Management</td>
+											<td>
+												<ul class="list-group list-group-flush">
+													<li class="list-group-item">CO1: To Understand General Concept & Problems Associated With Waste Management</li>
+													<li class="list-group-item">CO2: To Understand Onsite Handling, Storage & Processing Of Solid Waste</li>
+													<li class="list-group-item">CO3: To Understand Collection, Transfer & Transport Of Solid Waste</li>
+													<li class="list-group-item">CO3: To Understand Disposal Methods & Recovery Methods</li>
+												</ul>
+											</td>
+										</tr>
 		</tbody>
 	</table>
-	
 </section>
