a6364a35cfff5660190a29e56ac2039dd14993df
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-08-04 11:30:12 +0530

    homepage constructo name vol

--- departments/civil/modules/home.php
+++ departments/civil/modules/home.php
@@ -31,14 +31,14 @@
         <div class="sectionHeading">NEWS LETTER - CUNSTRUCTO</div>
         <div class="sectionContent card-body">
           <div class="row">
-            <div class="col-md-4 py-2"><a href="../departments/civil/src/newsletters/constructo-1.pdf" class="btn btn-block btn-lg btn-primary" download>Vol-1 Issue-1</a></div>
-            <div class="col-md-4 py-2"><a href="../departments/civil/src/newsletters/constructo-2.pdf" class="btn btn-block btn-lg btn-primary" download>Vol-1 Issue-2</a></div>
-            <div class="col-md-4 py-2"><a href="../departments/civil/src/newsletters/constructo-3.pdf" class="btn btn-block btn-lg btn-primary" download>Vol-2 Issue-1</a></div>
+            <div class="col-md-4 py-2"><a href="../departments/civil/src/newsletters/constructo-1.pdf" class="btn btn-block btn-lg btn-primary" download>Constructo Vol-1</a></div>
+            <div class="col-md-4 py-2"><a href="../departments/civil/src/newsletters/constructo-2.pdf" class="btn btn-block btn-lg btn-primary" download>Constructo Vol-2</a></div>
+            <div class="col-md-4 py-2"><a href="../departments/civil/src/newsletters/constructo-3.pdf" class="btn btn-block btn-lg btn-primary" download>Constructo Vol-3</a></div>
           </div>
           <hr>
           <p class="py-2 px-3">
-          Department of  Civil Engineering was established in year 2007. Department offers Bachelor of Technology degree with an intake of 150 students. We have well qualified and dedicated faculty and staff members. The dedication and hard work of the faculty and staff members have given fruitful results with our students securing ranks at University examination consistently.
-          Department have well equipped laboratories. A separate CENTRE OF EXCELLENECE has been established which give the opportunity to the students to work on the software’s such as  <strong>AutoCAD</strong>, <strong>Stadpro</strong>, <strong>C</strong>  etc. A wide selection of projects, seminars and training facilities besides technical visits to industrial enterprises and specialized lectures from industry specialists generates enormous potential and confidence among the students enabling them to independently handle the life size problems in their engineering careers.Many MoU’s are signed with various industries offering opportunity to the students to learn working culture of industry. The Department also has a well-established workshop for training in Moulding,  Smithy, Carpentry, Welding, Fitting and Machine tools.
+            Department of  Civil Engineering was established in year 2007. Department offers Bachelor of Technology degree with an intake of 150 students. We have well qualified and dedicated faculty and staff members. The dedication and hard work of the faculty and staff members have given fruitful results with our students securing ranks at University examination consistently.
+            Department have well equipped laboratories. A separate CENTRE OF EXCELLENECE has been established which give the opportunity to the students to work on the software’s such as  <strong>AutoCAD</strong>, <strong>Stadpro</strong>, <strong>C</strong>  etc. A wide selection of projects, seminars and training facilities besides technical visits to industrial enterprises and specialized lectures from industry specialists generates enormous potential and confidence among the students enabling them to independently handle the life size problems in their engineering careers.Many MoU’s are signed with various industries offering opportunity to the students to learn working culture of industry. The Department also has a well-established workshop for training in Moulding,  Smithy, Carpentry, Welding, Fitting and Machine tools.
           </p>
         </div>
       </section>
