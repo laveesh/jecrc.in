60560b80021a7f3e20776ef91ec9bef1a02ba71a
Laveesh Gupta <loveeshgoyal@gmail.com>
2018-08-11 13:59:25 +0530

    updated library

--- departments/it/modules/labs.php
+++ departments/it/modules/labs.php
@@ -4,26 +4,30 @@
 <p>The department has been provided with following laboratories and workshops fully equipped
 with equipment and machinery as prescribed in the syllabus.</p>
 <ul style="list-style-type: none;" class="sectionList">
-<li>Electronic Devices Circuit Lab</li>
-<li>Electronics Circuit Lab</li>
-<li>Pulse Digital Circuits Lab</li>
-<li>Analog Communication Lab</li>
-<li>Linear IC Applications Lab</li>
-<li>Microprocessors Microcontrollers Lab</li>
-<li>Digital Communication Lab</li>
-<li>Micro Processors Lab</li>
-<li>ECAD Lab</li>
-<li>Digital Signal Processing Lab</li>
-<li>Microwave Optical Communications Lab</li>
+<li>Data Structures and Algorithms Lab</li>
+<li>Object Oriented Programming</li>
+<li>Software Engineering Lab</li>
+<li>Linux Shell Programming Lab</li>
+<li>Database Management System Lab</li>
+<li>Network Programming Lab</li>
+<li>Java Programming Lab</li>
+<li>Web Technology Lab</li>
+<li>Operating Systems Simulation Lab</li>
+<li>GUI Design Lab.</li>
+<li>UML Lab</li>
+<li>Computer Graphics & Multimedia Lab.</li>
+<li>Data Mining and Warehousing Lab</li>
+<li>Internet Programming Lab.</li>
+<li>Software Testing Lab.</li>
+<li>Digital Image Processing lab</li>
+<li>Advanced Web Programming</li>
+<li>Mobile Application Development Lab</li>
 </ul>
 <div class="images"> 
 	<div class="row">
-	<div class="col-md-6 col-sm-12"><img class="img-thumbnail" src="../departments/ece/src/img/labs/cadLab1.jpg"></div>
- 	<div class="col-md-6 col-sm-12"><img class="img-thumbnail" src="../departments/ece/src/img/labs/cadLab2.jpg"></div>
+	<div class="col-md-6 col-sm-12"><img class="img-thumbnail" src="../departments/it/src/img/labs/lab1.jpg"></div>
+ 	<div class="col-md-6 col-sm-12"><img class="img-thumbnail" src="../departments/it/src/img/labs/lab2.jpg"></div>
  	</div>
- 	<div class="row">
-  	<div class="col-md-6 col-sm-12"><img class="img-thumbnail" src="../departments/ece/src/img/labs/de_lab.jpg"></div>
-   	<div class="col-md-6 col-sm-12"><img class="img-thumbnail" src="../departments/ece/src/img/labs/edc_lab.jpg"></div>
-   	</div>
+ 	
 </div>
 </section>
