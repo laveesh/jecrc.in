d1c07af2bc5642be2c22a74d4b5a596f2e62d752
amritsrivastava <srivastava.amrit1999@gmail.com>
2018-08-11 14:20:43 +0530

    update ece

--- departments/ece/modules/program_edu_obj.php
+++ departments/ece/modules/program_edu_obj.php
@@ -2,11 +2,11 @@
 	<div class="sectionHeading">Program Education Outcomes</div>
 	<div class="sectionContent">
 		<ul class="list-group list-group-flush">
-			<li class="list-group-item py-4"><strong>1. </strong>To provide students with the fundamentals of Engineering Sciences with more emphasis in Mechanical Engineering by way of analyzing and exploiting engineering challenges.</li>
-			<li class="list-group-item py-4"><strong>2. </strong>To train students with good scientific and engineering knowledge so as to comprehend, analyze, design, and create novel products and solutions for the real life problems.</li>
-			<li class="list-group-item py-4"><strong>3.</strong>To inculcate professional and ethical attitude, effective communication skills, teamwork skills, multidisciplinary approach, entrepreneurial thinking and an ability to relate engineering issues with social issues.</li>
-			<li class="list-group-item py-4"><strong>4. </strong>To provide students with an academic environment aware of excellence, leadership, written ethical codes and guidelines, and the self-motivated life-long learning needed for a successful professional career.</li>
-			<li class="list-group-item py-4"><strong>5. </strong>To prepare students to excel in Industry and Higher education by Educating Students along with High moral values and Knowledge.</li>
+			<li class="list-group-item py-4">1. To provide students with the fundamentals of engineering sciences with more emphasis in electronics &amp; communication engineering by way of analyzing and exploiting electronics engineering challenges.</li>
+			<li class="list-group-item py-4">2. To train students with good scientific and electronics &amp; communication engineering knowledge so as to comprehend, analyze, design, and create electronic based novel products and solutions for the real life problems.</li>
+			<li class="list-group-item py-4">3. To inculcate professional and ethical attitude, effective communication skills, teamwork skills, multidisciplinary approach, entrepreneurial thinking and an ability to relate electronics &amp; communication engineering with social issues.</li>
+			<li class="list-group-item py-4">4. To provide students with an academic environment aware of excellence, leadership, written ethical codes and guidelines, and the self-motivated life-long learning needed for a successful electronics &amp; communication engineering professional career.</li>
+			<li class="list-group-item py-4">5. To prepare students to excel in electronics &amp; communication based industry and higher education by educating students in electronics &amp; communication engineering field along with high moral values and knowledge.</li>
 		</ul>
 	</div>
 </section>
