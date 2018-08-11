9b849b85bbc51e2799290635e60ee0a178f39e0d
Laveesh Gupta <loveeshgoyal@gmail.com>
2018-08-11 14:01:58 +0530

    Laveesh it (#85)
    * co updated
    
    * publication updated
    
    * faculty updated
    
    * home updated
    
    * placement added
    
    * image fixed
    
    * sidenav edited
    
    * IT updateed
    
    * updated all the data
    
    * home updated
    
    * home photos
    
    * home photos
    
    * updated library

--- departments/it/modules/co_po_mapping.php
+++ departments/it/modules/co_po_mapping.php
@@ -1,5 +1,5 @@
 <section class="section container">
-	<div class="sectionHeading">Course Outcome</div>
+	<div class="sectionHeading">CO - PO Mapping</div>
 	<iframe src="http://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/it/src/pdf/CO-PO-Mapping.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>   
 
 	
