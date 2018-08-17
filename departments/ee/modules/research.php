4a598b766688814ed2253dfa70f2824e9fc30dc4
Arpit jain <maiarpitjain94@gmail.com>
2018-08-18 00:47:44 +0530

    student association (#91)
    * Merge branch 'master' into arpit-patch-1
    
    * faculty_awards , student_awards,co_po_maping,Course_outcome,Library
    
    * update me publications
    
    * ee faculty update pics remaining
    
    * publication update
    
    * update histoy, home
    
    * alumni update
    
    * update events, ug_programs
    
    * update alumni
    
    * update tech_events,laboratory
    
    * update stunt association

--- departments/ee/modules/research.php
+++ departments/ee/modules/research.php
@@ -1,21 +1,20 @@
 
 <!-- research home page -->
 <section class="section container card">
-  <div class="sectionHeading">Reasearch</div>
+  <div class="sectionHeading">Research</div>
   <div class="sectionContent card-body px-4">
-    <p>In the Electrical Department of JECRC, research activities are always the main focus of the department. The faculty members of the department pursue frontline research work in many areas of the broad field of Electrical Engineering. The research work is supported by sponsored projects from many government agencies as well as many private industries .The faculty members also collaborate with researchers in many industrial labs as well as academic institutions to pursue interdisciplinary topics. The department has also been actively involved in consultancy projects for industries. The department has many specialized laboratories to impart advanced training to research students.
-    </p>
 
-    <div class="sectionSubHeading">Reasearch areas</div>
+
+    <div class="sectionSubHeading py-4">The current research activities of the department can be broadly grouped into the following themes.</div>
 <!-- lists of researches -->
-<p>The current research activities of the department can be broadly grouped into the following themes.
-</p>
+
 <ol class="sectionList">
   <li class="py-2">Power Systems</li>
   <li class="py-2">Power Electronics</li>
   <li class="py-2">High Voltage Engineering</li>
   <li class="py-2">Systems Science and Signal Processing</li>
 </ol>
+
   </div>
 
 </section>
