668ac6753d7873e0d5d558b0a4c48ddf8ce5dc62
aditya81070 <aditya81070@gmail.com>
2018-08-18 14:10:01 +0530

    Update Contact

--- index.php
+++ index.php
@@ -71,14 +71,8 @@
             <li class="nav-item active">
               <a class="nav-link" href="pages/alumni.php">Alumni</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Contact Us
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="pages/contact_details.php">Contact Details</a>
-                <a class="dropdown-item" href="pages/how_to_reach.php">How to reach ?</a>
-              </div>
+            <li class="nav-item active">
+              <a class="nav-link" href="pages/contact.php">Contact Us</a>
             </li>
           </ul>
         </div>
