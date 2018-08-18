36a502023e0f27dfc2f68e5a93a9ac2335b7f097
aditya81070 <aditya81070@gmail.com>
2018-08-18 12:54:35 +0530

    Update newsleter name

--- departments/ee/modules/home.php
+++ departments/ee/modules/home.php
@@ -30,7 +30,7 @@
     </div>
     <div class="col-md-6">
       <section class="section container card">
-            <div class="sectionHeading">THE NEWSLETTER- E.MECHZINE</div>
+            <div class="sectionHeading">THE NEWSLETTER- TESLA</div>
             <div class="sectionContent card-body">
               <div class="row py-2">
                 <div class="col-md-4 py-2">
