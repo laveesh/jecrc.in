163561e3c8bd85454d3c56bc77648fbb447a8f2f
aditya81070 <aditya81070@gmail.com>
2018-08-20 10:54:45 +0530

    Small changes for ME

--- departments/cse/modules/contact.php
+++ departments/cse/modules/contact.php
@@ -2,9 +2,9 @@
 	<div class="sectionHeading">Contact Us</div>
 	<div class="sectionContent card-body">
 		<p>To request additional information concerning the Department of Computer Science and Engineering, you can reach us at:</p>
+		<h3>Prof. (Dr.) Vijay Singh Rathore</h3>
 		<h4 class="sectionSubHeading">CSE Head</h4>
 		<p>A-Block II Floor EDC Conference Room</p>
-<p>Phone:+91-9251039865</p>
 <p><a href="mailto:hod.cse@jecrc.ac.in">hod.cse@jecrc.ac.in</a></p>
 	</div>
 </section>
