5aa0e4c070a285b6c43435cebbb9a53b2967def0
aditya81070 <aditya81070@gmail.com>
2018-08-24 22:41:25 +0530

    Update Major Projects

--- departments/cse/modules/projects.php
+++ departments/cse/modules/projects.php
@@ -1,16 +1,10 @@
 
 <!-- research home page -->
 <section class="section container card">
-  <div class="sectionHeading">Reasearch</div>
+  <div class="sectionHeading">RESEARCH</div>
+  <div class="sectionHeading">MAJOR PROJECTS</div>
   <div class="sectionContent card-body px-4">
-    <p>
-      Diwanshu Soni, Ayush Khandelwal, Aman Vijay, Divyang Bhargava, Aniket Dixit and Apoorva Agrawal - Undergraduate Students of 5 sem Department of CSE, JECRC has developed the Anti Blue Whale Game i.e. "Try A-Gain...JINDA-DIL-KHEL" under the kind guidance of  Prof. (Dr.) Vijay Singh Rathore, Head- CSE & Ms. Shikha Maheshwari, Assistant Professor, CSE, JECRC which gives hundreds of reason to live the life with the smile while connecting users to their roots. The game is developed in an attempt to bring happiness to the users‟ daily life while encouraging positivity towards life through generous acts using the internet. It is an incredibly fun game that helps players in living every minute with a positive outlook, staying motivated, being happy and making others happy. This game app comes with a set of tasks that will instead have a positive effect on the player‟s life and helps the player to bring him/her closer to Self, Family, Friends, and Nation. The intention behind developing such game is to help the young minds in a positive way. The game focuses on spreading happy thoughts with its tasks and ends with a message that will bring a smile to anyone‟s face. The tasks of Try A-Gain app vary from easy workouts like morning walk to socially responsible tasks such as helping parents in household chores, etc. which eventually takes the player closer to the Family, Friends, and Nation and gives reason to live a better life
-    </p>
-    <p>
-      The Game app was launched on October 12, 2017 on Google PlayStore : 
-      <a href="https://play.google.com/store/apps/details?id=com.projects.diwanshusoni.try_a_gain" class="card-link">Play here</a>
-    </p>
-    <img src="../departments/cse/src/img/research/news.jpg" alt="" class="img-fluid card-img-bottom">
-  </div>
+    <iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/cse/src/pdf/majorProjects.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe> 
+    </div>
 
 </section>
