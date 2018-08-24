44cffe868e7d7282c368ed21ebad0a540e7060f0
aditya81070 <aditya81070@gmail.com>
2018-08-24 23:45:12 +0530

    Update RTU scheme

--- departments/cse/modules/ug_program.php
+++ departments/cse/modules/ug_program.php
@@ -1,42 +1,6 @@
-<div class="container card section">
-	<div class="sectionHeading">Undergraduate Program</div>
-	<div class="card-body sectionContent">
-		<p >Bachelor of Technology in Computer Science and Engineering or B.Tech. (CSE) is a full time under graduate programme, affiliated to Rajasthan Technical University, with a total duration 4 years, each year comprising of two semesters. It is one of the most sought after programme. All admissions to this programme are through RTU's State level Entrance Examination.</p>
-		<p >The course curriculum includes core courses along with professional as well as open elective courses. Courses from humanities, and social, basic and engineering sciences are also given due weightage as per standard norms.</p>
-		<h4 class="sectionSubHeading">Programme Educational Objectives</h4>
-		<ol class="sectionList">
-			<li>To provide students with the fundamentals of Engineering Sciences with more emphasis in Computer Science & Engineering by way of analyzing and exploiting engineering challenges.</li>
-			<li>To train students with good scientific and engineering knowledge so as to comprehend, analyze, design, and create novel products and solutions for the real life problems.</li>
-			<li>To inculcate professional and ethical attitude, effective communication skills, teamwork skills, multidisciplinary approach, entrepreneurial thinking and an ability to relate engineering issues with social issues.</li>
-			<li>To provide students with an academic environment aware of excellence, leadership, written ethical codes and guidelines, and the self-motivated life-long learning needed for a successful professional career.</li>
-			<li>To prepare students to excel in Industry and Higher education by Educating Students along with High moral values and Knowledge.</li>
-		</ol>
-		<h4 class="sectionSubHeading">Programme Outcomes</h4>
-		<p>After completion of the programme the students will possess the following abilities</p>
-		<ol class="sectionList">
-			<li><strong> Engineering knowledge: </strong>Apply the knowledge of mathematics, science, engineering fundamentals, and an engineering specialization to the solution of complex engineering problems.</li>
-			<li><strong> Problem analysis: </strong>Identify, formulate, research literature, and analyze complex engineering problems reaching substantiated conclusions using first principles of mathematics, natural sciences, and engineering sciences.</li>
-			<li><strong> Design/development of solutions: </strong>Design solutions for complex engineering problems and design system components or processes that meet the specified needs with appropriate consideration for the public health and safety, and the cultural, societal, and environmental considerations.</li>
-			<li><strong> Conduct investigations of complex problems: </strong>Use research-based knowledge and research methods including design of experiments, analysis and interpretation of data, and synthesis of the information to provide valid conclusions.</li>
-			<li><strong> Modern tool usage: </strong>Create, select, and apply appropriate techniques,
-			resources, and modern engineering and IT tools including prediction and
-			modeling to complex engineering activities with an understanding of the limitations.
-		</li>
-			<li><strong> The engineer and society: </strong>Apply reasoning informed by the contextual knowledge to assess societal, health, safety, legal and cultural issues and the consequent responsibilities relevant to the professional engineering practice.</li>
-			<li><strong>Environment and sustainability: </strong>Understand the impact of the professional engineering solutions in societal and environmental contexts, and demonstrate the knowledge of, and need for sustainable development.</li>
-			<li><strong>Ethics: </strong>Apply ethical principles and commit to professional ethics and responsibilities and norms of the engineering practice.</li>
-			<li><strong>Individual and team work: </strong>Function effectively as an individual, and as a member or leader in diverse teams, and in multidisciplinary settings.</li>
-			<li><strong>Communication: </strong>Communicate effectively on complex engineering activities with the engineering community and with society at large, such as, being able to comprehend and write effective reports and design documentation, make effective presentations, and give and receive clear instructions.</li>
-			<li><strong>Project management and finance: </strong>Demonstrate knowledge and
-			understanding of the engineering and management principles and apply
-			these to one’s own work, as a member and leader in a team, to manage
-			projects and in multidisciplinary environments.</li>
-			<li><strong>Life-long learning: </strong>Recognize the need for, and have the preparation and ability to engage in independent and life-long learning in the broadest context of technological change.</li>
-		</ol>
-		<h4 class="sectionSubHeading">Programme Specific Outcomes</h4>
-		<ul class="sectionList">
-			<li>Ability to interpret and analyze network specific and cyber security issues in real world environment.</li>
-			<li>Ability to design and develop Mobile and Web-based applications under realistic constraints.</li>
-		</ul>
+<section class="section container">
+	<div class="sectionHeading">UNDERGRADUATE PTROGRAMME</div>
+	<div class="sectionContent">
+		<iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/cse/src/pdf/CS-new-scheme-rtu-updated.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>   
 	</div>
-</div>
+</section>
