f5498edaf7d9fcb41417a54c00a9af4f891c771d
aditya81070 <aditya81070@gmail.com>
2018-08-29 11:15:08 +0530

    Placement added

--- departments/cse/modules/placements.php
+++ departments/cse/modules/placements.php
@@ -1,1 +1,5 @@
-<h1>this is placement</h1>
+<section class="section container">
+	<div class="sectionHeading">Computer Science Placement</div>
+	<div class="sectionHeading">Placements 2017-18</div>
+	<img src="../departments/cse/src/img/placement/placement17-18.png" alt="" class="img-fluid img-thumbnail">
+</section>
