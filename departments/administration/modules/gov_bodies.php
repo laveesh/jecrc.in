5c47c54e34122fc281ee11cdaf75d6061efb9456
Muskan1411 <muskan99ag@gmail.com>
2018-08-30 09:48:33 +0530

    governing bodies

--- departments/administration/modules/gov_bodies.php
+++ departments/administration/modules/gov_bodies.php
@@ -1,6 +1,7 @@
 <section class="section container card">
   <div class="sectionHeading">Anti-Ragging Committie</div>
   <div class="sectionContent card-body">
+  	<div class="table-responsive">
   	<table class="table table-bordered table-sm">
 		<thead class="thead-light">
 			<tr>
@@ -13,61 +14,61 @@
 		<tbody>
 			<tr>
 				<td>1.</td>
-				<td>sh. M.L.Sharma</td>
+				<td>Sh. M.L.Sharma</td>
 				<td>Chairman</td>
 				<td>F-30,MAJOR SHAITAN SINGH COLONY,SHASTRI NAGAR,JAIPUR-302016</td>
 			</tr>
 			<tr>
 				<td>2.</td>
-				<td>dr. Vinay Kumar Chandana</td>
+				<td>Dr. Vinay Kumar Chandana</td>
 				<td>Member Secretary</td>
 				<td>E-806,Asha deep apartment green avenue,jagatpura,jaipur-302027</td>
 			</tr>
 			<tr>
 				<td>3.</td>
-				<td>dr. umesh kumar pareek</td>
+				<td>Dr. Umesh Kumar Pareek</td>
 				<td>Member</td>
 				<td>CTS bus stand,sanganer,jaipur-302019</td>
 			</tr>
 			<tr>
 				<td>4.</td>
-				<td>sh. Manish jain</td>
+				<td>Sh. Manish Jain</td>
 				<td>Member</td>
 				<td>MALVIYA NAGAR,13/22,A,Jaipur-302019</td>
 			</tr>
 			<tr>
 				<td>5.</td>
-				<td>dr.naveen hemrajani</td>
+				<td>Dr.Naveen Hemrajani</td>
 				<td>Invited from other University</td>
 				<td></td>
 			</tr>
 			<tr>
 				<td>6.</td>
-				<td>nominee from the aicte</td>
+				<td>Nominee from the AICTE</td>
 				<td>(Ex-officio)</td>
 				<td>Regional office,plot no.1A,5th floor,buildingof directorate of technical education & industrial training,(govt. of punjab),sector-36A,chandigarh-160063 chandigarh</td>
 			</tr>
 			<tr>
 				<td>7.</td>
-				<td>an indrustrialist/technologist/eductionist from the region to be nominated by the state govt./ut</td>
+				<td>An indrustrialist/technologist/eductionist from the region to be nominated by the State Govt./UT</td>
 				<td>(Ex-officio)</td>
 				<td></td>
 			</tr>
 			<tr>
 				<td>8.</td>
-				<td>nominee of the state govt/ut</td>
+				<td>Nominee of the State Govt/UT</td>
 				<td>(Ex-officio)</td>
 				<td></td>
 			</tr>
 			<tr>
 				<td>9.</td>
-				<td>dr. rajesh singhal,professor</td>
+				<td>Dr. Rajesh Singhal,Professor</td>
 				<td>Member</td>
 				<td>RTU,akelgarh,rawatbhata road, kota</td>
 			</tr>
 			<tr>
 				<td>10.</td>
-				<td> indovision services pvt.ltd.authorized huawei network academy partner (HIT)</td>
+				<td> Indovision Services PVT.LTD.authorized huawei network academy partner (HIT)</td>
 				<td>Member(Invities)</td>
 				<td>3rd floor,centrum mall,khasra number 369,MG road, sultanpur,new delhi</td>
 			</tr>
@@ -80,17 +81,18 @@
 			</tr>
 			<tr>
 				<td>12.</td>
-				<td>forsk technologies private ltd.</td>
+				<td>Forsk Technologies Private LTD.</td>
 				<td>Member(Invities)</td>
 				<td>#M-5, software building,IT park, industrial area EPIP, Sitapura, jaipur</td>
 			</tr>
 			<tr>
 				<td>13.</td>
-				<td>CADD Centre Training Services pvt.ltd.,Chennai</td>
+				<td>CADD Centre Training Services Pvt.LTD.,Chennai</td>
 				<td>Member(Invities)</td>
 				<td>local office-106-107,mahima majesty,ram gali no.6, raja park, jaipur</td>
 			</tr>
 		</tbody>
 	</table>
+</div>
 </div>
 </section>
