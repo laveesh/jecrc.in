c85f55ff85ca7057ef37c4d30ca246091b675d40
Laveesh Gupta <loveeshgoyal@gmail.com>
2018-08-30 10:19:18 +0530

    it home updated

--- departments/it/modules/home.php
+++ departments/it/modules/home.php
@@ -1,111 +1,132 @@
-
-<!-- INFORMATION TECHNOLOGY HOME PAGE -->
-<div class="container-fluid">
+<section class="section container-fluid">
   <div class="row">
     <div class="col-md-3">
-      <section class="section container card">
-        <div class="sectionHeading">RECENT NEWS</div>
-        <div class="sectionContent card-body px-4 mh-75">
-        <marquee behavior="scroll" scrolldelay=60 scrollamount=4 direction="up" height=100%>
-        <ul class="list-group list-group-flush text-primary">
-          <li class="list-group-item">IT HACKATHON was conducted by IT department in November 2017</li>
-          <li class="list-group-item">IT IDEATHON was conducted by IT department in October 2017</li>
-          <li class="list-group-item">SIP-EXPO organised in November 2017</li>
-          <li class="list-group-item">Android Development Classes were taken by CAI group.</li>
-          <li class="list-group-item">Machine learning and IOT training conducted by Forks Technologies in March 2018</li>
-          <li class="list-group-item">Python and Web Development classes were taken by students.</li>
-          <li class="list-group-item">Sales Force Training conducted by Sales Force trainers for III year students.</li>
-        </ul>
-</marquee>
+      <div class="sectionHeading">RECENT NEWS</div>
+      <div class="h-section card sectionContent container">
+        <div class="card-body">
+                    <marquee behavior="scroll" scrolldelay=60 scrollamount=4 direction="up" height=100%>
+          <ul class="list-group list-group-flush text-primary">
+            <li class="list-group-item">
+              IT HACKATHON was conducted by IT department in November 2017
+            </li>
+            <li class="list-group-item">
+              IT IDEATHON was conducted by IT department in October 2017
+            </li>
+            <li class="list-group-item">
+              SIP-EXPO organised in November 2017
+            </li>
+            <li class="list-group-item">Android Development Classes were taken by CAI group.</li>
+            <li class="list-group-item">Machine learning and IOT training conducted by Forks Technologies in March 2018</li>
+            <li class="list-group-item">Python and Web Development classes were taken by students.</li>
+            <li class="list-group-item">Sales Force Training conducted by Sales Force trainers for III year students.</li>
+          </ul>
+          </marquee>
         </div>
-      </section>
+      
     </div>
+      </div>
+
     <div class="col-md-6">
-      <section class="section container card">
-        <div class="sectionHeading">NEWS LETTER & EDIFIER</div>
+      <div class="sectionHeading">NEWS LETTER & EDIFIER</div>
+      <div class="section container card h-section">
         <div class="sectionContent card-body">
-          <div class="row">
-            <div class="col-md-4 py-2 text-center">
-              <a href="../departments/it/src/newsletters/it_edifier.pdf" download>
-              <img src="../departments/it/src/img/newsletters/itedi.png" alt="" class="img-fluid img-thumbnail foo">
-            </a>
-          </div>
-          <div class="col-md-4 py-2 text-center">
-              <a href="../departments/it/src/newsletters/it_march.pdf" download><img src="../departments/it/src/img/newsletters/itmarch.png" alt="" class="img-fluid img-thumbnail foo">
-              </a>
-            </div>
-            <div class="col-md-4 py-2 text-center">
-              <a href="../departments/it/src/newsletters/it_april.pdf" download><img src="../departments/it/src/img/newsletters/itapril.png" alt="" class="img-fluid img-thumbnail foo">
-              </a>
-            </div>
-          </div>
-          <hr>
-          <p class="py-2 px-3">
-   The INFORMATION TECHNOLOGY department of JECRC Foundation has been on the pinnacle of all major achievements and developments in the field of computer sciences over the past few years. The incorrigible focus on developing fundamental and intellectual knowledge amongst the students through outcome based excellence in teaching has been exemplary.
+          <div class="owl-carousel owl-theme py-2 px-2">
+    <div class="item"><a href="../departments/it/src/newsletters/it_edifier.pdf" download><img src="../departments/it/src/img/newsletters/itedi.png" alt="" class="img-thumbnail"></a></div>
+    <div class="item"><a href="../departments/it/src/newsletters/it_march.pdf" download><img src="../departments/it/src/img/newsletters/itmarch.png" alt="" class="img-thumbnail"></a></div>
+    <div class="item"><a href="../departments/it/src/newsletters/it_april.pdf" download><img src="../departments/it/src/img/newsletters/itapril.png" alt="" class="img-thumbnail"></a></div>
+    
+</div>
+ <p class="px-2 pt-2">
+            The INFORMATION TECHNOLOGY department of JECRC Foundation has been on the pinnacle of all major achievements and developments in the field of computer sciences over the past few years. The incorrigible focus on developing fundamental and intellectual knowledge amongst the students through outcome based excellence in teaching has been exemplary.
 The department regularly arranges seminar, guest lectures and workshops by industry experts for students and staff. The department’s dedication towards it’s students has always set new benchmarks for other departments, by bringing in leading IT industries such as INFOSYS, TCS, MICROSOFT, AMDOCS, ACCENTURE, COGNIZANT, Thought works and NATIONAL ENTREPRENEURSHIP NETWORK AND signing MOU’S with them to promote INDUSTRY-INSTITUTE interaction.<br/>
 With every passing year the unpritdented success of IT students has been the highlight of the placement drives happening in the campus. Every year almost 20% of final year students secure admission for higher studies at renowned universities abroad and institutes of repute in India. The upsurge in the number of entrepreneurs from the campus in ritnt times has been really promising and their endeavors have really been galvanizing the students to take up start-up projects and achieve success.
 
           </p>
         </div>
-      </section>
+      </div>
     </div>
+   
     <div class="col-md-3">
-      <section class="section container card">
-        <div class="sectionHeading">STUDENT SPOTLIGHT</div>
-        <div class="sectionContent card-body">
-          <h3 class="sectionHeading">GATE Toppers</h3>
-
-
-
-          <div class="row">
-            <div class="col-md-6 text-center">
-              <img src="../departments/it/src/img/studentspotlight/pankaj.jpeg" alt="" class="img-fluid img-thumbnail rounded-circle">
-              <h5 class="py-2">Pankaj Soni</h5>
-            </div>
-            <div class="col-md-6 text-center">
-              <img src="../departments/it/src/img/studentspotlight/kamakhya.jpeg" alt="" class="img-fluid img-thumbnail rounded-circle">
-              <h5 class="py-2">Kamakhya Bansal</h5>
-            </div>
-            
-          </div>
-
-
-
-          <h3 class="sectionHeading">RTU Toppers</h3>
-
-
-
-          <div class="row">
-            <div class="col-md-4 text-center">
-              <img src="../departments/it/src/img/studentspotlight/priya.jpg" alt="" class="img-fluid img-thumbnail rounded-circle">
-              <h5 class="py-2">Priya Mishra</h5>
-            </div>
-            <div class="col-md-4 text-center">
-              <img src="../departments/it/src/img/studentspotlight/palak.jpg" alt="" class="img-fluid img-thumbnail rounded-circle">
-              <h5 class="py-2">Palak Saria</h5>
-            </div>
-            <div class="col-md-4 text-center">
-              <img src="../departments/it/src/img/studentspotlight/garima.jpg" alt="" class="img-fluid img-thumbnail rounded-circle">
-              <h5 class="py-2">Garima Mathur</h5>
-            </div>
-
-
-          </div>
-
-
-          <!-- <h3 class="sectionHeading">RTU Toppers</h3>
-          <div class="row">
-            <div class="col-md-12">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Priya Mishra</li>
-                <li class="list-group-item">Palak Saria</li>
-                <li class="list-group-item">Garima Mathur</li>
-              </ul>
-            </div>
-          </div> -->
+      <div class="sectionHeading">STUDENT SPOTLIGHT</div>
+      <div class="section h-section card container">
+        <div class="card-body sectionContent">
+          <div class="sectionHeading">Gate Qualifiers</div>
+        <div class="container">
+          
+            <ul class="list-group list-group-flush">
+          <li class="list-group-item">Pankaj Soni</li>
+          <li class="list-group-item">Kamakhya Bansal</li>
+        </ul>
+          
         </div>
-
-      </section>
+        </div>
+      </div>
     </div>
   </div>
-</div>
+</section>
+
+<section class="section container-fluid px-3">
+  <div class="sectionHeading">ACADMICS TOPPERS</div>
+  <div class="row">
+    <div class="col-md-6 text-center border-right">
+      <div class="sectionHeading">Odd Semester</div>
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <p class="text-center">Arpit Sharma</p>
+        </div>
+        <div class="col-md-6">
+          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <p class="text-center">Arpit Sharma</p>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <p class="text-center">Arpit Sharma</p>
+        </div>
+        <div class="col-md-6">
+          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <p class="text-center">Arpit Sharma</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 text-center">
+      <div class="sectionHeading">Even Semester</div>
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <img src="../departments/it/src/img/studentspotlight/priya.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <p class="text-center">Priya Mishra</p>
+        </div>
+        <div class="col-md-6">
+          <img src="../departments/it/src/img/studentspotlight/palak.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <p class="text-center">Palak Saria</p>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <img src="../departments/it/src/img/studentspotlight/garima.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <p class="text-center">Garima Mathur</p>
+        </div>
+        <div class="col-md-6">
+          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <p class="text-center">Arpit Sharma</p>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</section>
+
+
+
+
+<script>
+  $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    items: 3 
+    
+})
+</script>
