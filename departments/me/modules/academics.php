91b914a0b4fb77daabc6f331b871ebca60cdb22d
Prasang Gupta <31915135+Prasang007@users.noreply.github.com>
2018-08-31 21:29:45 +0530

    final ME page (#109)
    * conflict resolve work last
    
    * typos corrected ME
    
    * final ME update

--- departments/me/modules/academics.php
+++ departments/me/modules/academics.php
@@ -2,19 +2,19 @@
 	<div class="sectionHeading">Academics</div>
 	<div class="card-body sectionContent">
 	<p>
-		The Department is affiliated to Rajasthan Technical University and offers Under-Graduate programmes. 
+		The Department is affiliated to Rajasthan Technical University and offers Under-Graduate programmes.
 	</p>
 	<p>
-		The ME Department practices the Outcome Based Education (OBE) approach for teaching-learning process. This is a learning-centric approach rather than traditional teaching-centric approach to education. This approach focuses on the attributes the students should possess after the completion of the programme through the following key questions:- 
+		The ME Department practices the Outcome-Based Education (OBE) approach for the teaching-learning process. This is a learning-centric approach rather than a traditional teaching-centric approach to education. This approach focuses on the attributes the students should possess after the completion of the programme through the following key questions:-
 	</p>
 	<ol class="sectionList">
 		<li>At the end of the course, what should a student be able to do?</li>
 		<li>How can the faculty and staff help the student in gaining this ability?</li>
 		<li>How will we measure if the students have gained this ability?</li>
-		<li>How to involve all the stakeholders viz. students, faculty, alumni, employers, industry, parents and society, for continuous improvement of the programme?</li>
+		<li>How to involve all the stakeholders viz. students, faculty, alumni, employers, industry, parents, and society, for continuous improvement of the programme?</li>
 	</ol>
 <p>
-	Under OBE model, the quality of teaching is measured by the quality of learning, defined as outcomes, that takes place. Programme Educational Objectives (PEOs) are formulated to align with the vision and mission of the Department. Programme Outcomes (POs) are defined to achieve PEOs which are further aligned to Course Outcomes (CO) of each subject. Vision, Mission and PEOs are the long-term outcomes whereas POs and COs are the short-term outcomes
+Under OBE model, the quality of teaching is measured by the quality of learning, defined as outcomes, that takes place. Programme Educational Objectives (PEOs) are formulated to align with the vision and mission of the Department. Programme Outcomes (POs) are defined to achieve PEOs which are further aligned to Course Outcomes (CO) of each subject. Vision, Mission, and PEOs are the long-term outcomes whereas POs and COs are the short-term outcomes
 </p>
 <div class="text-center">
 	<img src="../departments/me/src/img/Academics/acadmics.png" alt="acadmics" class="rounded">
--- departments/me/modules/mission_vision.php
+++ departments/me/modules/mission_vision.php
@@ -3,14 +3,14 @@
 	<div class="sectionContent card-body">
 	<h3 class="sectionSubHeading">Vision</h3>
 	<p>
-		The Mechanical Engineering Department strives to be recognized globally for outcome based technical knowledge and to produce quality human resource, who can manage the advance technologies and contribute to society.
+		The Mechanical Engineering Department strives to be recognized globally for outcome-based technical knowledge and to produce a quality human resource, who can manage the advanced technologies and contribute to society.
 	</p>
 	<h3 class="sectionSubHeading">Mission
 		</h3>
 		<ol class="list-group list-group-flush sectionList">
 			<li class="list-group-item py-3">To impart quality technical knowledge to the learners to make them globally competitive mechanical engineers.</li>
-			<li class="list-group-item py-3">To provide the learners ethical guidelines along with excellent academic environment for a long productive career.</li>
-			<li class="list-group-item py-3">To promote industry-institute relationship.</li>			
+			<li class="list-group-item py-3">To provide the learners ethical guidelines along with an excellent academic environment for a long productive career.</li>
+			<li class="list-group-item py-3">To promote the industry-institute relationship.</li>			
 		</ol>
 	
 	</div>
