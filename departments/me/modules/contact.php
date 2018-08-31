c33c967e86af9c87bdc4e001eb027208735c8f1b
prasang007 <prasang00@gmail.com>
2018-08-31 20:54:41 +0530

    typos corrected ME

--- departments/me/modules/contact.php
+++ departments/me/modules/contact.php
@@ -6,5 +6,7 @@
 		<h4 class="sectionSubHeading">Mechanical HOD</h4>
 		<p>B-Block</p>
 		<p><a href="mailto:hod.me@jecrc.ac.in">hod.me@jecrc.ac.in</a></p>
+		<img src="../departments/me/src/img/about/D_BLock.jpg" alt="" class="img-fluid img-thumbnail"><hr>
+		Picture of Mechanical block (D-Block).
 	</div>
 </section>
