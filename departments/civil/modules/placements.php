f51cabc5c736fa7482079d5d4efccf38dd2f2de2
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-09-01 02:55:59 +0530

    placement pdfs

--- departments/civil/modules/placements.php
+++ departments/civil/modules/placements.php
@@ -1,2 +1,23 @@
-Placements
-
+<section class="section container card">
+	<div class="sectionHeading">Placements @Civil Department</div>
+	<div class="sectionContent card-body">
+        <div class="row">
+            <section class="section container">
+                <div class="sectionHeading">2017-18</div>
+                <iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/civil/src/pdf/placements3.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>
+            </section>
+        </div>
+        <div class="row">
+            <section class="section container">
+                <div class="sectionHeading">2016-17</div>
+                <iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/civil/src/pdf/placements2.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>
+            </section>
+        </div>
+        <div class="row">
+            <section class="section container">
+                <div class="sectionHeading">2015-16</div>
+                <iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/civil/src/pdf/placements1.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>
+            </section>
+        </div>
+    </div>
+</section>
