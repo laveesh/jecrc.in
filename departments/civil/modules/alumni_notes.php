5dc868b5a5d6bf93480d2589ab38388a86cc24b7
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-08-31 10:09:26 +0530

    alumni w/o photos

--- departments/civil/modules/alumni_notes.php
+++ departments/civil/modules/alumni_notes.php
@@ -1,50 +1,66 @@
 <div class="container section">
 
 <div class="sectionContent">
+	<div class="card">
+		<div class="card-header text-center">
+			<strong>RADHIKA BHARDWAJ</strong> 
+		</div>
 	<div class="row">
 		<div class="col-md-3">
 			<!-- <img src="../departments/civil/src/img/alumni/radhika.jpg"> -->
 		</div>
 		<div class="col-md-9 text-center">
-			<strong>RADHIKA BHARDWAJ</strong> 
-			<p>I believe that choosing JECRC for my studies was a lifetime decision. The teaching learning process is friendly besides being efficient. I am grateful to my faculty and management for providing me with the required industrial exposure which is helpful for me at my workplace. The proper nurturing at JECRC has helped me a lot professionally.</p>
+			<p class="px-3">I believe that choosing JECRC for my studies was a lifetime decision. The teaching learning process is friendly besides being efficient. I am grateful to my faculty and management for providing me with the required industrial exposure which is helpful for me at my workplace. The proper nurturing at JECRC has helped me a lot professionally.</p>
 		</div>
+	</div>
 	</div>
 </div>
 
 <div class="sectionContent">
-	<div class="row">
-		<div class="col-md-3">
-			<img src="../departments/civil/src/img/alumni/kalpit.jpg">
-		</div>
-		<div class="col-md-9 text-center">
+	<div class="card">
+		<div class="card-header text-center">
 			<strong>KALPIT MEENA</strong>
-			<p>I am fortunate enough to be a JECRC Alumni as I had ample opportunities at the college for my academic excellence as well as personality grooming. JECRC is one place where dream meets reality. The moments captured at JECRC are the most precious for me. I thank the faculty and management for giving us a life changing experience.</p>
 		</div>
-	</div>
-</div>
-
-<div class="sectionContent">
 	<div class="row">
 		<div class="col-md-3">
-			<!-- <img src="../departments/civil/src/img/alumni/nikhil.jpg"> -->
+			<!-- <img src="../departments/civil/src/img/alumni/kalpit.jpg"> -->
 		</div>
 		<div class="col-md-9 text-center">
+			<p class="px-3">I am fortunate enough to be a JECRC Alumni as I had ample opportunities at the college for my academic excellence as well as personality grooming. JECRC is one place where dream meets reality. The moments captured at JECRC are the most precious for me. I thank the faculty and management for giving us a life changing experience.</p>
+		</div>
+	</div>
+	</div>
+</div>
+
+<div class="sectionContent">
+	<div class="card">
+		<div class="card-header text-center">
 			<strong>NIKHIL MAHESWARI</strong>
-			<p>The college days at JECRC were filled with joy and enthusiasm. This was due to the dedicated college management and faculty of CE department. The practical experience gained through industrial tours and visits helped us to evolve as a civil engineer. I feel privileged to be a JECRC Alumni.</p>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+				<!-- <img src="../departments/civil/src/img/alumni/nikhil.jpg"> -->
+			</div>
+			<div class="col-md-9 text-center">
+				<p class="px-3">The college days at JECRC were filled with joy and enthusiasm. This was due to the dedicated college management and faculty of CE department. The practical experience gained through industrial tours and visits helped us to evolve as a civil engineer. I feel privileged to be a JECRC Alumni.</p>
+			</div>
 		</div>
 	</div>
 </div>
 
 <div class="sectionContent">
+	<div class="card">
+		<div class="card-header text-center">
+			<strong>ANSHUL KUMAR GUPTA</strong>
+		</div>
 	<div class="row">
 		<div class="col-md-3">
 			<!-- <img src="../departments/civil/src/img/alumni/anshul.jpg"> -->
 		</div>
 		<div class="col-md-9 text-center">
-			<strong>ANSHUL KUMAR GUPTA</strong>
-			<p>The availability of right opportunity at right time is very important. I think getting admitted to JECRC was an opportunity met at the right time for me. I dedicate my professional achievements to the honest efforts of the JECRC family. I believe it is an organization which believes in all round development of its students. I thank CE department faculty for their commendable efforts in making me an efficient civil engineer. </p>
+			<p class="px-3">The availability of right opportunity at right time is very important. I think getting admitted to JECRC was an opportunity met at the right time for me. I dedicate my professional achievements to the honest efforts of the JECRC family. I believe it is an organization which believes in all round development of its students. I thank CE department faculty for their commendable efforts in making me an efficient civil engineer. </p>
 		</div>
+	</div>
 	</div>
 </div>
 
