c0cd41d7b354189b8eb5295b829236820b0abbd8
Laveesh Gupta <loveeshgoyal@gmail.com>
2018-09-01 09:20:43 +0530

    Laveesh it (#111)
    * co updated
    
    * publication updated
    
    * faculty updated
    
    * home updated
    
    * placement added
    
    * image fixed
    
    * sidenav edited
    
    * IT updateed
    
    * updated all the data
    
    * home updated
    
    * home photos
    
    * home photos
    
    * updated library
    
    * spell check
    
    * complete
    
    * hone page content
    
    * navbar changed
    
    * sidenav fixed
    
    * it home updated
    
    * content added
    
    * update newsletter
    
    * home updated
    
    * carousel updated
    
    * projects added

--- departments/it/modules/home.php
+++ departments/it/modules/home.php
@@ -4,62 +4,69 @@
       <div class="sectionHeading">RECENT NEWS</div>
       <div class="h-section card sectionContent container">
         <div class="card-body">
-                    <marquee behavior="scroll" scrolldelay=60 scrollamount=4 direction="up" height=100%>
-          <ul class="list-group list-group-flush text-primary">
-            <li class="list-group-item">
-              IT HACKATHON was conducted by IT department in November 2017
-            </li>
-            <li class="list-group-item">
-              IT IDEATHON was conducted by IT department in October 2017
-            </li>
-            <li class="list-group-item">
-              SIP-EXPO organised in November 2017
-            </li>
-            <li class="list-group-item">Android Development Classes were taken by CAI group.</li>
-            <li class="list-group-item">Machine learning and IOT training conducted by Forks Technologies in March 2018</li>
-            <li class="list-group-item">Python and Web Development classes were taken by students.</li>
-            <li class="list-group-item">Sales Force Training conducted by Sales Force trainers for III year students.</li>
-          </ul>
+          <marquee behavior="scroll" scrolldelay=60 scrollamount=4 direction="up" height=100%>
+            <ul class="list-group list-group-flush text-primary">
+              <li class="list-group-item">
+                IT HACKATHON was conducted by IT department in November 2017
+              </li>
+              <li class="list-group-item">
+                IT IDEATHON was conducted by IT department in October 2017
+              </li>
+              <li class="list-group-item">
+                SIP-EXPO organised in November 2017
+              </li>
+              <li class="list-group-item">Android Development Classes were taken by CAI group.</li>
+              <li class="list-group-item">Machine learning and IOT training conducted by Forks Technologies in March 2018</li>
+              <li class="list-group-item">Python and Web Development classes were taken by students.</li>
+              <li class="list-group-item">Sales Force Training conducted by Sales Force trainers for III year students.</li>
+            </ul>
           </marquee>
         </div>
-      
-    </div>
+
       </div>
+    </div>
 
     <div class="col-md-6">
       <div class="sectionHeading">NEWS LETTER & EDIFIER</div>
       <div class="section container card h-section">
         <div class="sectionContent card-body">
           <div class="owl-carousel owl-theme py-2 px-2">
-    <div class="item"><a href="../departments/it/src/newsletters/it_edifier.pdf" download><img src="../departments/it/src/img/newsletters/itedi.png" alt="" class="img-thumbnail"></a></div>
-    <div class="item"><a href="../departments/it/src/newsletters/it_march.pdf" download><img src="../departments/it/src/img/newsletters/itmarch.png" alt="" class="img-thumbnail"></a></div>
-    <div class="item"><a href="../departments/it/src/newsletters/it_april.pdf" download><img src="../departments/it/src/img/newsletters/itapril.png" alt="" class="img-thumbnail"></a></div>
-    
-</div>
- <p class="px-2 pt-2">
+            <div class="item"><a href="../departments/it/src/newsletters/it_april.pdf" download><img src="../departments/it/src/img/newsletters/itapril.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/itjuly.pdf" download><img src="../departments/it/src/img/newsletters/itjuly.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/it_edifier.pdf" download><img src="../departments/it/src/img/newsletters/itedi.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/itmarch17.pdf" download><img src="../departments/it/src/img/newsletters/itmarch17.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/itaug17.pdf" download><img src="../departments/it/src/img/newsletters/itaug17.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/itsept17.pdf" download><img src="../departments/it/src/img/newsletters/itsept17.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/itoct17.pdf" download><img src="../departments/it/src/img/newsletters/itoct17.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/itjan18.pdf" download><img src="../departments/it/src/img/newsletters/itjan18.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/itfeb18.pdf" download><img src="../departments/it/src/img/newsletters/itfeb18.jpg" alt="" class="img-thumbnail"></a></div>
+            <div class="item"><a href="../departments/it/src/newsletters/it_march.pdf" download><img src="../departments/it/src/img/newsletters/itmarch.jpg" alt="" class="img-thumbnail"></a></div>
+
+          </div>
+          <p class="px-2 pt-2">
             The area of Information technology (IT) deals with the use of computers to store, retrieve, transmit, and manipulate data or information in the context of various fields.There are multiple and lucrative positions available in the information technology industry. It is one of the fastest growing employment industries in the world. <br/>
-Over the years, the department of IT at JECRC Foundation has created an aura for its innovative short-term courses through continuing education. The department has always provided such environment in which new ideas, research and scholarship flourish and from which the innovators of tomorrow emerge.
-Through rigorous coursework and by providing an understanding of the needs of industry, we aim towards new technological trends as well as industrial interactions. <br/  >
-With every passing year the unpritdented success of IT students has been the highlight of the placement drives happening in the campus. Every year almost 20% of final year students secure admission for higher studies at renowned universities abroad and institutes of repute in India. The upsurge in the number of entrepreneurs from the campus in ritnt times has been really promising and their endeavors have really been galvanizing the students to take up start-up projects and achieve success.
+            Over the years, the department of IT at JECRC Foundation has created an aura for its innovative short-term courses through continuing education. The department has always provided such environment in which new ideas, research and scholarship flourish and from which the innovators of tomorrow emerge.
+            Through rigorous coursework and by providing an understanding of the needs of industry, we aim towards new technological trends as well as industrial interactions. <br/  >
+            With every passing year the unpritdented success of IT students has been the highlight of the placement drives happening in the campus. Every year almost 20% of final year students secure admission for higher studies at renowned universities abroad and institutes of repute in India. The upsurge in the number of entrepreneurs from the campus in ritnt times has been really promising and their endeavors have really been galvanizing the students to take up start-up projects and achieve success.
 
           </p>
         </div>
       </div>
     </div>
-   
+
     <div class="col-md-3">
       <div class="sectionHeading">STUDENT SPOTLIGHT</div>
       <div class="section h-section card container">
         <div class="card-body sectionContent">
           <div class="sectionHeading">Gate Qualifiers</div>
-        <div class="container">
-          
+          <div class="container">
+
             <ul class="list-group list-group-flush">
-          <li class="list-group-item">Pankaj Soni</li>
-          <li class="list-group-item">Kamakhya Bansal</li>
-        </ul>
-          
-        </div>
+              <li class="list-group-item">Pankaj Soni</li>
+              <li class="list-group-item">Kamakhya Bansal</li>
+            </ul>
+
+          </div>
         </div>
       </div>
     </div>
@@ -124,10 +131,10 @@ With every passing year the unpritdented success of IT students has been the hig
 
 <script>
   $('.owl-carousel').owlCarousel({
-    loop:false,
+    loop:true,
     margin:10,
     nav:true,
     items: 3 
     
-})
+  })
 </script>
