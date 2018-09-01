458496f00ddb580834cdc650f2fadbf1260a603d
Laveesh Gupta <loveeshgoyal@gmail.com>
2018-09-01 12:22:45 +0530

    toppers added

--- departments/it/modules/home.php
+++ departments/it/modules/home.php
@@ -4,28 +4,49 @@
       <div class="sectionHeading">RECENT NEWS</div>
       <div class="h-section card sectionContent container">
         <div class="card-body">
-          <marquee behavior="scroll" scrolldelay=60 scrollamount=4 direction="up" height=100%>
-            <ul class="list-group list-group-flush text-primary">
-              <li class="list-group-item">
-                IT HACKATHON was conducted by IT department in November 2017
-              </li>
-              <li class="list-group-item">
-                IT IDEATHON was conducted by IT department in October 2017
-              </li>
-              <li class="list-group-item">
-                SIP-EXPO organised in November 2017
-              </li>
-              <li class="list-group-item">Android Development Classes were taken by CAI group.</li>
-              <li class="list-group-item">Machine learning and IOT training conducted by Forks Technologies in March 2018</li>
-              <li class="list-group-item">Python and Web Development classes were taken by students.</li>
-              <li class="list-group-item">Sales Force Training conducted by Sales Force trainers for III year students.</li>
-            </ul>
+          <marquee behavior="scroll" scrolldelay=60 scrollamount=6 direction="up" height=100% >
+          <ul class="list-group list-group-flush text-primary">
+            <li class="list-group-item">
+              Induction program for 2017-2021 batch from 1<sup>st</sup> July to 15<sup>th</sup> July 2018
+            </li>
+            <li class="list-group-item">
+              Placement training for final year students from 9<sup>th</sup> July 2018
+            </li>
+            <li class="list-group-item">
+              Hands on workshop on Docker on 10<sup>th</sup> August & 11<sup>th</sup> August 2018
+            </li>
+            <li class="list-group-item">
+              2 Days workshop on aptitude skills and personality developer on 18<sup>th</sup> August & 20<sup>th</sup> August 2018
+            </li>
+            <li class="list-group-item">
+              Organising IT hackathon on 12<sup>th</sup> September 2018
+            </li>
+            <li class="list-group-item">
+              Pre hackathon workshop on introduction to hackathon on 20<sup>th</sup> August 2018
+            </li>
+            <li class="list-group-item">
+              Pre hackathon workshop on introduction to Git on 22<sup>nd</sup> August 2018
+            </li>
+            <li class="list-group-item">
+              Pre hackathon workshop on introduction to Web Development on 30<sup>th</sup> August 2018
+            </li>
+            <li class="list-group-item">
+              Pre hackathon workshop on introduction to Android on 31<sup>st</sup> August 2018
+            </li>
+            <li class="list-group-item">
+              Pre hackathon workshop on introduction to Git - II on 4<sup>th</sup> September 2018
+            </li>
+            <li class="list-group-item">
+              Organising Ideathon on 17<sup>th</sup> October 2018
+            </li>
+            <li class="list-group-item">
+              Organising IT hackathon on 2<sup>nd</sup> November 2018
+            </li>
+          </ul>
           </marquee>
         </div>
-
       </div>
     </div>
-
     <div class="col-md-6">
       <div class="sectionHeading">NEWS LETTER & EDIFIER</div>
       <div class="section container card h-section">
@@ -41,100 +62,120 @@
             <div class="item"><a href="../departments/it/src/newsletters/itjan18.pdf" download><img src="../departments/it/src/img/newsletters/itjan18.jpg" alt="" class="img-thumbnail"></a></div>
             <div class="item"><a href="../departments/it/src/newsletters/itfeb18.pdf" download><img src="../departments/it/src/img/newsletters/itfeb18.jpg" alt="" class="img-thumbnail"></a></div>
             <div class="item"><a href="../departments/it/src/newsletters/it_march.pdf" download><img src="../departments/it/src/img/newsletters/itmarch.jpg" alt="" class="img-thumbnail"></a></div>
-
           </div>
           <p class="px-2 pt-2">
             The area of Information technology (IT) deals with the use of computers to store, retrieve, transmit, and manipulate data or information in the context of various fields.There are multiple and lucrative positions available in the information technology industry. It is one of the fastest growing employment industries in the world. <br/>
             Over the years, the department of IT at JECRC Foundation has created an aura for its innovative short-term courses through continuing education. The department has always provided such environment in which new ideas, research and scholarship flourish and from which the innovators of tomorrow emerge.
             Through rigorous coursework and by providing an understanding of the needs of industry, we aim towards new technological trends as well as industrial interactions. <br/  >
             With every passing year the unpritdented success of IT students has been the highlight of the placement drives happening in the campus. Every year almost 20% of final year students secure admission for higher studies at renowned universities abroad and institutes of repute in India. The upsurge in the number of entrepreneurs from the campus in ritnt times has been really promising and their endeavors have really been galvanizing the students to take up start-up projects and achieve success.
-
           </p>
         </div>
       </div>
     </div>
-
     <div class="col-md-3">
       <div class="sectionHeading">STUDENT SPOTLIGHT</div>
       <div class="section h-section card container">
         <div class="card-body sectionContent">
           <div class="sectionHeading">Gate Qualifiers</div>
           <div class="container">
-
             <ul class="list-group list-group-flush">
               <li class="list-group-item">Pankaj Soni</li>
+              <li class="list-group-item">Pramod Nagar</li>
               <li class="list-group-item">Kamakhya Bansal</li>
             </ul>
-
+          </div>
+          <div class="sectionHeading">Acadmics Toppers</div>
+          <div class="container">
+            <div class="row justify-content-center text-center">
+              <div class="col-md-6">
+                <img src="../departments/it/src/img/studentspotlight/laveesh.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+                <p class="text-center">Laveesh Gupta</p>
+                <p class="text-center">3<sup>rd</sup> Semister</p>
+              </div>
+              <div class="col-md-6">
+                <img src="../departments/it/src/img/studentspotlight/kunal.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+                <p class="text-center">Kunal Mishra</p>
+                <p class="text-center">5<sup>th</sup> Semister</p>
+              </div>
+              <div class="col-md-6">
+                <img src="../departments/it/src/img/studentspotlight/vatsal.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+                <p class="text-center">Vatsal Babel</p>
+                <p class="text-center">6<sup>th</sup> Semister</p>
+              </div>
+              <div class="col-md-6">
+                <img src="../departments/it/src/img/studentspotlight/ishmeet.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+                <p class="text-center">Ishmeet Kaur</p>
+                <p class="text-center">7<sup>th</sup> & 8<sup>th</sup> Semister</p>
+              </div>
+              
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </section>
-
 <section class="section container-fluid px-3">
-  <div class="sectionHeading">ACADMICS TOPPERS</div>
+  <div class="sectionHeading">RTU TOPPERS</div>
   <div class="row">
+    <div class="col-md-2 text-center border-right">
+      <div class="sectionHeading">2015</div>
+      <div class="row justify-content-center">
+        <div class="col-md-12">
+          <img src="../departments/it/src/img/studentspotlight/ankita.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <div class="py-2">
+            <p class="text-center">Ankita Makwana</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 text-center border-right">
+      <div class="sectionHeading">2016</div>
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <img src="../departments/it/src/img/studentspotlight/chetna.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <div class="py-2">
+            <p class="text-center">Chetna Chotrani</p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../departments/it/src/img/studentspotlight/kirti.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
+          <div class="py-2">
+            <p class="text-center">Kirti Aggarwal</p>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="col-md-6 text-center border-right">
-      <div class="sectionHeading">Odd Semester</div>
+      <div class="sectionHeading">2017</div>
       <div class="row justify-content-center">
-        <div class="col-md-6">
-          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
-          <p class="text-center">Arpit Sharma</p>
-        </div>
-        <div class="col-md-6">
-          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
-          <p class="text-center">Arpit Sharma</p>
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
-          <p class="text-center">Arpit Sharma</p>
-        </div>
-        <div class="col-md-6">
-          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
-          <p class="text-center">Arpit Sharma</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6 text-center">
-      <div class="sectionHeading">Even Semester</div>
-      <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-4">
           <img src="../departments/it/src/img/studentspotlight/priya.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
-          <p class="text-center">Priya Mishra</p>
+          <div class="py-2">
+            <p class="text-center">Priya Mishra</p>
+          </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
           <img src="../departments/it/src/img/studentspotlight/palak.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
-          <p class="text-center">Palak Saria</p>
+          <div class="py-2">
+            <p class="text-center">Palak Saria</p>
+          </div>
         </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-4">
           <img src="../departments/it/src/img/studentspotlight/garima.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
-          <p class="text-center">Garima Mathur</p>
-        </div>
-        <div class="col-md-6">
-          <img src="../departments/cse/src/img/people/faculty/AbhishekDixit.jpg" alt="" class="img-fluid img-thumbnail rounded-circle topper">
-          <p class="text-center">Arpit Sharma</p>
+          <div class="py-2">
+            <p class="text-center">Garima Mathur</p>
+          </div>
         </div>
       </div>
     </div>
-    
   </div>
 </section>
-
-
-
-
 <script>
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    items: 3 
-    
-  })
+$('.owl-carousel').owlCarousel({
+loop:true,
+margin:10,
+nav:true,
+items: 3
+})
 </script>
