e08c691eed282168a153136aa720f027f174aedf
Prasang Gupta <31915135+Prasang007@users.noreply.github.com>
2018-09-01 13:21:37 +0530

    ME update (#113)
    * conflict resolve work last
    
    * typos corrected ME
    
    * final ME update
    
    * ME placement update
    
    * Resolve conflict of D_block.JPG
    
    * ME homepage update
    
    * ME small change

--- departments/me/modules/sidenav.php
+++ departments/me/modules/sidenav.php
@@ -65,13 +65,13 @@
                   <a class="dropdown-item" href="me.php?section=staff">Technical Staff</a>
                 </div>
               </li>
-              <li class="active"><a href="me.php?section=tech_events">Events<span class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+              <li class="active"><a href="me.php?section=events">Events<span class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
               <li class="nav-item dropdown">
                 <a class="dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Awards & Achievements
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="me.php?section=events">Events</a>
+                  <a class="dropdown-item" href="me.php?section=tech_events">Technical Events</a>
                   <a class="dropdown-item" href="me.php?section=awards_faculty">Faculties Awards</a>
                   <a class="dropdown-item" href="me.php?section=awards_student">Students Awards</a>
                 </div>
