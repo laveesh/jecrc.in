9f28cbe6b02ea0264645320b3250bc4bf141a68a
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-09-01 23:40:45 +0530

    alumni library

--- departments/civil/modules/library.php
+++ departments/civil/modules/library.php
@@ -1,5 +1,5 @@
 <section class="section container card">
-	<div class="sectionHeading">Departement Libraray</div>
+	<div class="sectionHeading">Department Libraray</div>
 	<div class="sectionContent card-body">
 		<ul class="list-group list-group-flush">
 			<li class="list-group-item"><strong>Staff in charge: </strong> </li>
@@ -9,6 +9,9 @@
 <div class="row">
 <section class="section container">
 	<div class="sectionHeading">Library</div>
+	<div class="container-fluid mx-auto my-2">
+		<img class="offset-3" src="../departments/civil/src/img/library/library.jpg">
+	</div>
 	<iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/civil/src/pdf/library.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>
 </section>
 </div>
