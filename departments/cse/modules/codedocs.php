a51c53041a7a23e257f6c0ab2b3ccb5de1600634
Laveesh Gupta <loveeshgoyal@gmail.com>
2018-09-02 15:59:25 +0530

    most content added

--- departments/cse/modules/codedocs.php
+++ departments/cse/modules/codedocs.php
@@ -1,30 +1,29 @@
 <section class="section container">
-		<div class="sectionHeading">CodeDocs</div>
+	<div class="sectionHeading">CodeDocs</div>
 	<div class="sectionContent">
 		<div class="px-2">
 			<blockquote>
-			<strong>Learn, Code, Contribute, Collaborate.</strong>
-		</blockquote>
-		<p>Codedocs is open source student coding community of JECRC, completely managed and operated by students thriving to bring change.</p>
-		<p>Throughout the year, students organise courses (Android, Web, Data Science etc) , events, workshops, competitions, contests etc in different domains.</p>
-		<p>
-			CodeDocs transform students to industry experts through skills, practical programming, and working in teams. Students work together, network and learn, and solve some real challenges.
-		</p>
-		<p>
-			CodeDocs major initiatives: 
-		</p>
-	<ul class="sectionList initiatives">
-		<li>Courses in trending skills.</li>
-		<li>Workshops and sessions on latest developer tools.</li>
-		<li>Working collaboratively on Open Source projects.</li>
-		<li>Coding Contests and Hackathons.</li>
-		<li>Informative sessions on various opportunities like Google Summer of Code.</li>
-		<li>Peer to Peer interaction, learning and talent showcase through "Open Spaces".</li>
-	</ul>
-	<p>
-		With CodeDocs, students of JECRC work together and enhance their skills to become better Software developers while enjoying to Code 🎉 🎉.
-	</p>
+				<strong>Learn, Code, Contribute, Collaborate.</strong>
+			</blockquote>
+			<p>Codedocs is open source student coding community of JECRC, completely managed and operated by students thriving to bring change.</p>
+			<p>Throughout the year, students organise courses (Android, Web, Data Science etc) , events, workshops, competitions, contests etc in different domains.</p>
+			<p>
+				CodeDocs transform students to industry experts through skills, practical programming, and working in teams. Students work together, network and learn, and solve some real challenges.
+			</p>
+			<p>
+				CodeDocs major initiatives: 
+			</p>
+			<ul class="sectionList initiatives">
+				<li>Courses in trending skills.</li>
+				<li>Workshops and sessions on latest developer tools.</li>
+				<li>Working collaboratively on Open Source projects.</li>
+				<li>Coding Contests and Hackathons.</li>
+				<li>Informative sessions on various opportunities like Google Summer of Code.</li>
+				<li>Peer to Peer interaction, learning and talent showcase through "Open Spaces".</li>
+			</ul>
+			<p>
+				With CodeDocs, students of JECRC work together and enhance their skills to become better Software developers while enjoying to Code 🎉 🎉.
+			</p>
 		</div>
-		</div>
-	
+	</div>
 </section>
