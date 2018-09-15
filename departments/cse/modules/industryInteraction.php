583119b28656beb429a57e9dce6998c677970a49
Aditya Agarwal <your-email-address>
2018-09-15 10:50:33 +0530

    Add feedback forms

---
+++ departments/cse/modules/industryInteraction.php
@@ -0,0 +1,9 @@
+<section class="section container">
+	<div class="sectionHeading">FEEDBACK FORM</div>
+	<div class="row">
+		<div class="col-md-12">
+			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdq7VVocDbs79c6OtzOZHdQecJ98xIqw5_3BkxVz1_kLmpcog/viewform?c=0&w=1" width="100%" height="1200" frameborder="0" allowfullscreen>Loading...</iframe>
+		</div>
+	</div>
+	
+</section>
