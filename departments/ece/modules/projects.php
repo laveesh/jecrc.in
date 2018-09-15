46ddde0107b488e3cd6943bb2e6b4eb954888aa3
aish51197 <aish51197@gmail.com>
2018-09-15 10:29:48 +0530

    changes in ece

--- departments/ece/modules/projects.php
+++ departments/ece/modules/projects.php
@@ -2,7 +2,7 @@
 <!-- research home page -->
 <section class="section container card">
   <div class="sectionHeading">RESEARCH</div>
-  <div class="sectionHeading">DST PROJECTS</div>
+  <div class="sectionHeading">MAJOR PROJECTS</div>
   <div class="sectionContent card-body px-4">
     <iframe src="https://docs.google.com/gview?url=https://jecrcwebsite.herokuapp.com/departments/ece/src/pdf/majorprojects.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>
   </div>
