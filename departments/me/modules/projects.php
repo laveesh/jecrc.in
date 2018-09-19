f2baafbeed06ecea054f6c9818e63e09addbc540
Prasang Gupta <31915135+Prasang007@users.noreply.github.com>
2018-09-19 15:33:21 +0530

    me near final (#120)

--- departments/me/modules/projects.php
+++ departments/me/modules/projects.php
@@ -1,7 +1,7 @@
 <section class="section container card">
 	<div class="sectionHeading">Projects</div>
 	<div class="sectionContent">
-		<div class="sectionHeading">DST </div>
-		<iframe src="https://docs.google.com/gview?url=https://jecrc.in/departments/me/src/pdf/dst_data.pdf&embedded=true" class="embeded-doc me-newsletter" width="100%" frameborder="0"></iframe>
+		<!-- <div class="sectionHeading">DST </div> -->
+		<!-- <iframe src="https://docs.google.com/gview?url=https://jecrc.in/departments/me/src/pdf/dst_data.pdf&embedded=true" class="embeded-doc me-newsletter" width="100%" frameborder="0"></iframe> -->
 	</div>
 </section>
