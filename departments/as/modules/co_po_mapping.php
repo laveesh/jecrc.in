f37a2a2aac1d064dfda599fdf56d1e3e92e39439
Nurul Hassan <33507767+nurulhassan@users.noreply.github.com>
2018-10-03 22:34:36 +0530

    Add files via upload

---
+++ departments/as/modules/co_po_mapping.php
@@ -0,0 +1,5 @@
+<section class="section container card">
+  <div class="sectionHeading">Course Outcomes and Mapping</div>
+<!--  <embed src="../departments/me/src/pdf/books_database.pdf" class="embed-doc" value="FitH" type='application/pdf'> -->
+  <iframe src="https://docs.google.com/gview?url=https://jecrc.in/departments/as/src/pdf/copomapp.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>   
+</section>
