efe9ce1c70a169e8bea5f1c74ac18611c4eb1dfc
nurulhassan <nurulhassan2000@gmail.com>
2018-10-03 22:06:29 +0530

    as updated

--- departments/as/src/js/as.js
+++
@@ -1,9 +0,0 @@
-let params = (new URL(document.location)).searchParams;
-let id = params.get("section");
-$ ( document ).ready(() => {
-    var url="../departments/as/modules/"+id+".php"
-    $('#as_root').load(url);
-});
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
-});
