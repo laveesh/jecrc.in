1f787ddd94ee2eaf394ea3bf5f510033b8be017b
Nurul Hassan <33507767+nurulhassan@users.noreply.github.com>
2018-10-03 22:22:22 +0530

    Nurul patch1 (#129)
    * cse updated and applied science department added
    
    * cse updated and applied science department added
    
    * cse placement updated
    
    * as updated
    
    * Update sidenav.php
    
    * as faculty updated
    
    * as updated
    
    * Delete sidenav.php
    
    * Delete faculty.php

--- departments/as/modules/about.php
+++
@@ -1,9 +0,0 @@
-<section class="section container card">
-	<div class="sectionHeading">About Us</div>
-	<div class="sectionContent card-body px-4">
-		<p>
-			The Departments of Electronics & Communication Engineering are the major departments involving number of laboratories with a diversified variety of equipment being the core branches and with laboratories focusing on fundamental aspects of Electronics & Communication Engineering. In order to cope up with the requirement of industries, the departments also have laboratories with latest technologies. Students have an open access in the laboratories, to understand as well as apply their knowledge to explore their engineering skills. The departments have team of highly motivated and dedicated faculty members to the cause of academics and striving to do the best in the interest of the college and the students. Most of the faculty members are actively involved in research work and regularly publishing their research papers in Journals and Conferences. 
-
-		</p>
-	</div>
-</section>
--- departments/as/modules/pso.php
+++
@@ -1,9 +0,0 @@
-<section class="section container">
-			<div class="sectionHeading">PROGRAM SPECIFIC OUTCOMES</div>
-			<div class="sectionContent px-2 py-1">
-				<ul class="sectionList list-group list-group-flush ">
-					<li class="list-group-item"> An ability to apply the concepts of embedded systems and its applications.</li>
-			<li class="list-group-item">Ability to apply Field Programmable Gate Array (FPGA) based applications.</li>
-				</ul>
-			</div>
-		</section>
