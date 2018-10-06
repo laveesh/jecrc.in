720d0c8ef66c31d58bff0e0414c9fa9a8afbb613
nurulhassan <nurulhassan2000@gmail.com>
2018-10-06 13:36:52 +0530

    merge fix

--- departments/as/modules/student_associations.php
+++
@@ -1,7 +0,0 @@
-<div class="container section">
-	<div class="sectionHeading"> STUDENT ASSOCIATIONS</div>
-	<h4 class="sectionSubHeading">Xananoids</h4>
-
-	<div class="sectionContent">In this race of just coming first and not grasping any knowledge, Xananoids took its initial step in inculcating the knowledge among the students of the first year with the basic of robotics. The seminar took place in three sessions. It was great to see such overwhelming enthusiasm of the young buds. We wish that this zeal and curiosity learn more and more stays forever. The members of the Xananoids club go to various technical fests in a different event. Recently the team took part in plinth tech fest of LNMIIT in the transporter and stood second in it. The team consist of Mithlesh (ECE) Rishabh (ECE) Harish (Mech) Gaurav (Mech) Jitendra (Mech) and Shubham (CSE).
-	</div>
-</div>
