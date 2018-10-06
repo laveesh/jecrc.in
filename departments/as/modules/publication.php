db1938a5ba4d527601ffce6db8ea81c4ca507e89
nurulhassan <nurulhassan2000@gmail.com>
2018-10-06 13:33:08 +0530

    merge fix

---
+++ departments/as/modules/publication.php
@@ -0,0 +1,8 @@
+<!-- research publication page -->
+<section class="section container card">
+  <div class="sectionHeading">Publications</div>
+  <div class="sectionContent card-body px-4">
+<iframe src="https://docs.google.com/gview?url=https://jecrc.in/departments/ece/src/pdf/publication.pdf&embedded=true" class="embed-doc" frameborder="0"></iframe>
+
+  </div>
+</section>
