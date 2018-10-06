f0db69669faaf6e3dad614235540ab700b7c3640
Nurul Hassan <33507767+nurulhassan@users.noreply.github.com>
2018-10-06 13:40:57 +0530

    Nurul patch1 (#131)
    * cse updated and applied science department added
    
    * cse updated and applied science department added
    
    * cse placement updated
    
    * as updated
    
    * Update sidenav.php
    
    * as faculty updated
    
    * as updated
    
    * Delete sidenav.php
    
    * Delete faculty.php
    
    * as updated
    
    * merge fix
    
    * merge fix
    
    * merge fix

---
+++ placements/modules/2006_08.php
@@ -0,0 +1,12 @@
+<section class="section container">
+	<div class="sectionHeading">Statistics 2008</div>
+	<img src="../departments/placements/src/img/stats/2008.jpg" alt="Stats 2008" class="stats-img">
+</section>
+<section class="section container">
+	<div class="sectionHeading">Statistics 2007</div>
+	<img src="../departments/placements/src/img/stats/2007.jpg" alt="Stats 2007" class="stats-img">
+</section>
+<section class="section container">
+	<div class="sectionHeading">Statistics 2006</div>
+	<img src="../departments/placements/src/img/stats/2006.jpg" alt="Stats 2006" class="stats-img">
+</section>
---
+++ placements/modules/facilities.php
@@ -0,0 +1,30 @@
+<section class="section container">
+	<div class="sectionHeading">JECRC-Centre for Deep Learning</div>
+	<div class="sectionContent">
+		<p>As the competition to grab the best job opportunities in the field gets fierce, JECRC has acknowledged that traditional class room teaching is not enough to provide our students with the cutting edge they need to supersede their peers. Adapting to the changing paradigms of education deliverance, JECRC has tied up with many agencies to provide in-house training to our students on the latest technologies in demand in both IT and core sector. These trainings are provided on campus and at discounted charges and the trainers are industry experts. On completion of these trainings students can also go for certification and have yet another skill to be mentioned in their resume.</p>
+	</div>
+	<div class="sectionSubHeading">Salesforce Training Module</div>
+	<div class="sectionContent">
+		<p>Salesforce Training module was conducted at JECRC from 17 th January to 23 rd March 2018 wherein a total of 110 hours was dedicated to train students on 5 modules viz. Introduction, Customer Relationship Management, Administration, Development &amp; Lightning. A total of 112 students were trained in this duration. The module has been beneficial in improving the technical knowledge of our students in the fields of informational organization, CRM for enhanced communication, automation of everyday tasks, improved analytical data and reporting. Out of the 112 students, 105 received salesforce badge and 7 received salesforce super badge.</p>
+	</div>
+	<div class="sectionSubHeading">Embedded &amp; Robotics Design Training</div>
+	<div class="sectionContent">
+		<p>Embedded &amp; Robotics Design Training was conducted at JECRC in collaboration with Sakrobotix Research &amp; Startup Centre from 19 th January to 22 nd January 2018 wherein a total of 28 hours was dedicated to train 77 students. On successful completion the students received certification from Sakrobotix and IIT Bhubneshwar. The students were trained on external hardware interfacing and 8051 microcontrollers functioning and architecture. Level 2 training will start in next phase next year with updated and higher technologies syllabus like (Raspberry Pi, Arduino, ARM processor and IOT etc.).</p>
+	</div>
+	<div class="sectionSubHeading">Forsk Technologies</div>
+	<div class="sectionContent">
+		<p>JECRC has signed MoU with FORSK Technology. The objective of this MoU is to bring industry approach of solution development and product engineering to engineering candidates through project based learning through project based learning backed by data and technology. Project-based learning is a dynamic classroom approach in which students actively explore real-world problems and challenges and acquire a deeper knowledge. FORSK will offer project based learning in IoT (Internet of Things) and Machine Learning (Data Science) to JECRC students.</p>
+	</div>
+	<div class="sectionSubHeading">TechieNest</div>
+	<div class="sectionContent">
+		<p>During this training students would be trained in Embedded Systems &amp; Robotics and they would be also aware about IoT technology. The detailed contents covered during this 90 hours program. After completion of this training, students would be prepared to design Voice Control Robot, SPY Robot Gesture Control Wheel chair, GSM Based Notice Board, Multi-Layer Security System, SMART Home Automation System (Voice, Remote, Smart Phones, Internet etc) RFID based Attendance System using Local Database, Maze Robo, Grid Control Robo, Line Follower Robo, Password access. It will help them in grabbing various job opportunities in MNCs.</p>
+	</div>
+	<div class="sectionSubHeading">CADD Software</div>
+	<div class="sectionContent">
+		<p>JECRC in collaboration with CADD-CENTRE under National Skill Development Corporation (NSDC), Ministry of Skills set up by Govt. of India, offers affordable CAD (Auto Cad/Solid works/ANSYS) training for individuals.  As CADD CENTRE is a reputable providers of CAD skills training, will teach students to develop genuine CAD skills which will be appreciated by employers. Students will know how to simulate and validate the performance of products of all manufacturing sectors including automotive and to simulate every structural aspect, including linear static analysis of a single part of a complex assembly with hundreds of components interacting through contacts or relative motions.</p>
+	</div>
+	<div class="sectionSubHeading">Linux by Red Hat</div>
+	<div class="sectionContent">
+		<p>JECRC has organized training on Linux by RedHat which itself is a renowned name in the field of IT-Sector. RedHat have started RHCSA free of cost training program in which various students of CSE, IT, ECE and EE have participated. RHCSA is an entry-level certification that focuses on actual competencies at system administration, including installation and configuration of a Red Hat Enterprise Linux system and attach it to a live network running network services. After completion of training students will get prepared for RHCSA (Red Hat Certified System Administrator) Exam and after this exam they will be RHCSA certified which will help them in grabbing various job opportunities.</p>
+	</div>
+</section>
