47dda1c10b2a38cdf2c2b1a8043bc4c462ff09f0
nurulhassan <nurulhassan2000@gmail.com>
2018-10-22 19:19:10 +0530

    me project added

--- departments/me/modules/projects.php
+++ departments/me/modules/projects.php
@@ -1,7 +1,40 @@
 <section class="section container card">
 	<div class="sectionHeading">Projects</div>
 	<div class="sectionContent">
-		<!-- <div class="sectionHeading">DST </div> -->
-		<!-- <iframe src="https://docs.google.com/gview?url=https://jecrc.in/departments/me/src/pdf/dst_data.pdf&embedded=true" class="embeded-doc me-newsletter" width="100%" frameborder="0"></iframe> -->
+		<div class="sectionHeading">DST </div>
+		<div class="owl-carousel owl-theme">
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst1.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst2.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst3.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst4.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst5.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst6.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst7.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst8.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst9.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst10.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst11.png" alt="" class="img-fluid"></div>
+    <div class="item"><img src="../departments/me/src/img/projects/dst/dst12.png" alt="" class="img-fluid"></div>
+    
+    
+         </div>
 	</div>
 </section>
+<script>
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+</script>
