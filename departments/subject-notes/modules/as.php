6033051d4e19ae1eb475035f0901bce1db943356
Laveesh Gupta <loveeshgoyal@gmail.com>
2020-06-23 20:10:18 +0530

    Ad basic page and side bar for subject notes

---
+++ departments/subject-notes/modules/as.php
@@ -0,0 +1,44 @@
+<section class="section container-fluid">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="sectionHeading">Subject List</div>
+      <div class="h-section card sectionContent container">
+        <div class="card-body">
+          <ul class="list-group list-group-flush text-primary">
+            <li class="list-group-item">Team "Krypton" from IT Department, Jecrc Foundation Won TEQIP-III RTU SKIT Hackathon 2019
+            </li>
+            <li class="list-group-item">Seminar held on Cardiopulmonary Resuscitation at JECRC Foundation
+            </li>
+            <li class="list-group-item">Workshop on "Project Based Learning" was conducted by I.T Department on 15 December At JECRC Foundation</li>
+            <li class="list-group-item">IT Ideathon 2.0 was conducted by I.T Department on 10 December At JECRC Foundation</li>
+            <li class="list-group-item">I.T. Hackathon 2.0 was conducted by I.T Department on 12 September</li>
+            <li class="list-group-item">
+              Induction program for 2017-2021 batch from 1<sup>st</sup> July to 15<sup>th</sup> July 2018
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-9">
+      <div class="sectionHeading">Subject Notes</div>
+      <div class="section container sectionContent card">
+        <div class="card-body">
+          <p class="px-2 pt-2">
+            The area of Information technology (IT) deals with the use of computers to store, retrieve, transmit, and manipulate data or information in the context of various fields.There are multiple and lucrative positions available in the information technology industry. It is one of the fastest growing employment industries in the world. <br/>
+            Over the years, the department of IT at JECRC Foundation has created an aura for its innovative short-term courses through continuing education. The department has always provided such environment in which new ideas, research and scholarship flourish and from which the innovators of tomorrow emerge.
+            Through rigorous coursework and by providing an understanding of the needs of industry, we aim towards new technological trends as well as industrial interactions. <br/  >
+            With every passing year the unpritdented success of IT students has been the highlight of the placement drives happening in the campus. Every year almost 20% of final year students secure admission for higher studies at renowned universities abroad and institutes of repute in India. The upsurge in the number of entrepreneurs from the campus in ritnt times has been really promising and their endeavors have really been galvanizing the students to take up start-up projects and achieve success.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<script>
+$('.owl-carousel').owlCarousel({
+loop:true,
+margin:10,
+nav:true,
+items: 3
+})
+</script>
---
+++ pages/subject-notes.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>JECRC</title>
+  <link rel="stylesheet" href="../src/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../src/css/bootstrap.css">
+  <link rel="stylesheet" href="../src/css/style.css">
+  <link rel="stylesheet" href="../departments/subject-notes/src/css/subject-notes.css">
+  <link rel="icon" href="../src/img/favicon.png" type="image/x-icon"/>
+
+<link rel="stylesheet" href="../src/css/owl.carousel.min.css">
+<link rel="stylesheet" href="../src/css/owl.theme.default.min.css">
+<script src="../src/js/jquery.min.js"></script>
+    <script src="../src/js/popper.min.js"></script>
+    <script src="../src/js/bootstrap.min.js"></script>
+    <script src="../src/js/owl.carousel.min.js"></script>
+    <script src="../departments/subject-notes/src/js/subject-notes.js"></script>
+</head>
+<body>
+  <?php include '../modules/header.php'; ?>
+
+  <?php include '../departments/subject-notes/modules/sidenav.php'; ?>
+
+  <main id="notes_root">
+    
+  </main>
+
+  <?php include '../modules/footer.php'; ?>
+</body>
+</html>
