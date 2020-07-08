d752cf2be6234949e5b8eed10d720eea0c411cb7
Laveesh Gupta <loveeshgoyal@gmail.com>
2020-07-09 05:16:45 +0530

    Add new sql dump

--- dashboard/jecrc.sql
+++ dashboard/jecrc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 23, 2020 at 08:52 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
+-- Generation Time: Jul 09, 2020 at 01:45 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,9 +29,63 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `department` (
-  `dpt_id` int(11) NOT NULL,
-  `dpt_name` varchar(20) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `name`) VALUES
+(1, 'First Year'),
+(2, 'Computer Science and Engineering'),
+(3, 'Information Technology'),
+(4, 'Mechanical Engineering'),
+(5, 'Electronics and Communication Engineering'),
+(6, 'Civil Engineering'),
+(7, 'Electrical Engineering');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab`
+--
+
+CREATE TABLE `lab` (
+  `id` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8mb4 NOT NULL,
+  `dpt_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lab`
+--
+
+INSERT INTO `lab` (`id`, `name`, `dpt_id`) VALUES
+(2, 'Sample 2', 2),
+(3, 'Sample 3', 1),
+(4, 'Sample 1', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab_assigned`
+--
+
+CREATE TABLE `lab_assigned` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `lab_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lab_assigned`
+--
+
+INSERT INTO `lab_assigned` (`id`, `user_id`, `lab_id`) VALUES
+(2, 12, 4),
+(3, 12, 3);
 
 -- --------------------------------------------------------
 
@@ -40,10 +94,11 @@ CREATE TABLE `department` (
 --
 
 CREATE TABLE `notes` (
-  `notes_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `sub_assign_id` int(11) NOT NULL,
   `topic` varchar(20) NOT NULL,
-  `link` varchar(20) NOT NULL
+  `link` varchar(200) NOT NULL,
+  `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -53,22 +108,41 @@ CREATE TABLE `notes` (
 --
 
 CREATE TABLE `subject` (
-  `sub_id` int(11) NOT NULL,
-  `sub_name` varchar(30) NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `dpt_id` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`id`, `name`, `dpt_id`) VALUES
+(2, 'Object Oriented Programming with C++', 3),
+(3, 'Operating System', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sub_assisned`
+-- Table structure for table `sub_assigned`
 --
 
-CREATE TABLE `sub_assisned` (
+CREATE TABLE `sub_assigned` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `sub_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sub_assigned`
+--
+
+INSERT INTO `sub_assigned` (`id`, `user_id`, `sub_id`) VALUES
+(4, 10, 0),
+(5, 10, 0),
+(8, 12, 2),
+(11, 12, 3),
+(13, 10, 2);
 
 -- --------------------------------------------------------
 
@@ -78,11 +152,11 @@ CREATE TABLE `sub_assisned` (
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `user_name` varchar(20) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
   `user_password` varchar(20) NOT NULL,
   `user_role` varchar(20) NOT NULL,
-  `user_email` varchar(20) NOT NULL
+  `user_email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -90,8 +164,23 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `user_name`, `user_password`, `user_role`, `user_email`) VALUES
-(1, 'admin', '', '123', 'admin', 'examole@gmail.com'),
-(2, 'hb', '', '123', 'teacher', 'example@gmail.com');
+(1, 'admin', 'Admin', '123', 'admin', 'examole@gmail.com'),
+(10, 'hb', 'Good name', '123', 'teacher', 'example@example.com'),
+(12, 'tech.it', 'IT Editor', '1234', 'editor', 'example@example.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `lab_assign_id` int(11) NOT NULL,
+  `topic` text CHARACTER SET utf8 NOT NULL,
+  `code` varchar(100) NOT NULL,
+  `description` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -101,24 +190,36 @@ INSERT INTO `users` (`user_id`, `username`, `user_name`, `user_password`, `user_
 -- Indexes for table `department`
 --
 ALTER TABLE `department`
-  ADD PRIMARY KEY (`dpt_id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lab`
+--
+ALTER TABLE `lab`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lab_assigned`
+--
+ALTER TABLE `lab_assigned`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `notes`
 --
 ALTER TABLE `notes`
-  ADD PRIMARY KEY (`notes_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `subject`
 --
 ALTER TABLE `subject`
-  ADD PRIMARY KEY (`sub_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `sub_assisned`
+-- Indexes for table `sub_assigned`
 --
-ALTER TABLE `sub_assisned`
+ALTER TABLE `sub_assigned`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -128,6 +229,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -135,31 +242,49 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `dpt_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `lab`
+--
+ALTER TABLE `lab`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `lab_assigned`
+--
+ALTER TABLE `lab_assigned`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `notes_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `sub_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `sub_assisned`
+-- AUTO_INCREMENT for table `sub_assigned`
 --
-ALTER TABLE `sub_assisned`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `sub_assigned`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
